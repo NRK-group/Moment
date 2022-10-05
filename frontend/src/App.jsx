1862048a1d021dca7ade91ce8486e7755bd3e758
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-05 16:57:15 +0100

    add header layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './layouts/Header/Header';
 
 function App() {
-  return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <Header />
+        </div>
+    );
 }
 
 export default App;

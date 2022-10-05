37f66a7226340374c1d2cadebfb2b906f094984c
nater68 <n.d.russell@hotmail.com>
2022-10-05 15:14:08 +0100

    Login box started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
+import Login from './pages/login/Login';
 
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>
+    <Login />
+  </div>;
 }
 
 export default App;

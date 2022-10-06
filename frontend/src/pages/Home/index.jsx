1b08b1a8cf96b1b48bc9504d3c565749bb7f0287
keivon <keivon.991@gmail.com>
2022-10-06 15:55:10 +0100

    add Avatar

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,16 +1,41 @@
+
 import './Home.css';
+import Avatar from '../../components/Avatar';
+
 
 function Home() {
+
+
     return (
         <div className='homePage'>
-            <h2>Home</h2>
+       
             <div className='homePageContainer'>
                 <div>
-                    <div className='homePageStory'>Storys</div>
-                    <div className='homePagePostArea'>
-                        Post
+                    <div className='homePageStory'>
+                       
+                    <Avatar
+                            avatarSrc={
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
+                            }
+                            styleName={'AvatarUserProfile'}
+                            name={'Profile'}
+                            avatarIcon={"AvatarUserProfile"}
+                        />
+                 
+                        
+                        {
+                            [1,2,3,4,5,6,7].map(ele=>(
+                        <Avatar
+                            avatarSrc={
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
+                            }
+                            styleName={'AvatarUsers'}
+                            name={'ele'}
+                            key={ele}
+                        />
+))}
                     </div>
-
+                    <div className='homePagePostArea'>Post</div>
                 </div>
                 <div className='homePageProfile'>homePageProfile</div>
             </div>

199ce08e1e75485decd2dc4922738b80b0d38da2
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-06 16:45:08 +0100

    23-create-header-navbar-for-mobile-view

--- frontend/src/components/Icons/Icons.jsx
+++ frontend/src/components/Icons/Icons.jsx
@@ -67,4 +67,47 @@ const GroupsIcon = () => {
         </Icon>
     );
 };
-export { HomeIcon, SearchIcon, NewPostIcon, MessagesIcon, GroupsIcon };
+const NotificationsIcon = () => {
+    return (
+        <Icon>
+            <svg
+                className='icon'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 448 512'>
+                <path d='M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64 208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416H416c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z' />
+            </svg>
+        </Icon>
+    );
+};
+const UserIcon = () => {
+    return (
+        <Icon>
+            <svg
+                className='icon'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 448 512'>
+                <path d='M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z' />
+            </svg>
+        </Icon>
+    );
+};
+const ProfileIcon = ({ img }) => {
+    return (
+        <>
+            {img ? (
+                <img className='profileIcon' src={img} alt='profile' />
+            ) : (
+                <UserIcon />
+            )}
+        </>
+    );
+};
+export {
+    HomeIcon,
+    SearchIcon,
+    NewPostIcon,
+    MessagesIcon,
+    GroupsIcon,
+    NotificationsIcon,
+    ProfileIcon,
+};

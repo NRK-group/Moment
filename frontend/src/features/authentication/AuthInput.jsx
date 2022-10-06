86689fe3a08b896765a7fc6d502bb8b3599df5ef
nater68 <n.d.russell@hotmail.com>
2022-10-06 17:18:35 +0100

    Updating registration profile image

--- frontend/src/features/authentication/AuthInput.jsx
+++ frontend/src/features/authentication/AuthInput.jsx
@@ -1,6 +1,6 @@
 export default function AuthInput(props) {
   return (
-    <div class="inputBox">
+    <div className="inputBox">
       <input type={props.type} className={props.styleName} required />
       <span className='inputText' >{props.placeholder}</span>
     </div>

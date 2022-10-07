b1e1b0a721526dfb0be37e1f8deeb49075ce6258
keivon <keivon.991@gmail.com>
2022-10-07 20:23:41 +0100

    add dropdown menu on the post

--- frontend/src/features/Post/index.jsx
+++ frontend/src/features/Post/index.jsx
@@ -1,31 +1,58 @@
-import Card from "../../components/card/Card"
-import Avatar from "../../components/Avatar"
-import './Post.css'
-
-
+import Card from '../../components/card/Card';
+import Avatar from '../../components/Avatar';
+import './Post.css';
+import { useRef, useState } from 'react';
 
 export default function Post(props) {
-    return (
-        <Card styleName={'PostContainer'} >
- <Card styleName={"PostHeader"}>
- 
-<Avatar
-   avatarSrc={
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
+
+
+    const dropdown = useRef(null)
+    const [toggle, setToggle] = useState(true);
+
+const OpenDropdownMenu = ()=> {
+    setToggle(!toggle)
+    if(toggle){
+        console.log("inside")
+    dropdown.current.style.display = "block"
+    } else {
+        dropdown.current.style.display = "none"
+    }
 }
-styleName={'PostAvatarUsers'}
-/>
 
-<p>name</p>
 
- </Card>
-    <Card styleName={"PostBody"} >
-<p>fdrfrfre</p>
-    </Card>
-    <Card styleName={"PostContent"} >
-<p>fdrfrfre</p>
-    </Card>
-    </Card>
-    )
-  }
-  
+    return (
+        <>
+            <Card styleName={'PostContainer'}>
+                <Card styleName={'PostHeader'}>
+                    <>
+                        <Avatar
+                            avatarSrc={
+                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
+                            }
+                            styleName={'PostAvatarUsers'}
+                        />
+
+                        <p>name</p>
+                    </>
+
+                    <div className='PostHeaderMenu'>
+                        <button className='dropbtn' onClick={()=> OpenDropdownMenu()}>
+                            <i className='fa-solid fa-ellipsis' />
+                        </button>
+                        <div ref={dropdown} className='dropdown-content'>
+                            <a href='#'>option 1</a>
+                            <a href='#'>option 2</a>
+                            <a href='#'>option 3</a>
+                        </div>
+                    </div>
+                </Card>
+                <Card styleName={'PostBody'}>
+                    <p>fdrfrfre</p>
+                </Card>
+                <Card styleName={'PostContent'}>
+                    <p>fdrfrfre</p>
+                </Card>
+            </Card>
+        </>
+    );
+}
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,45 +1,42 @@
-
 import './Home.css';
 import Avatar from '../../components/Avatar';
 import Post from '../../features/Post';
 
-
 function Home() {
-
-
     return (
         <div className='homePage'>
-       
+            <br />
             <div className='homePageContainer'>
                 <div>
                     <div className='homePageStory'>
-                       
-                    <Avatar
+                        <Avatar
                             avatarSrc={
                                 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
                             }
                             styleName={'AvatarUserProfile'}
                             name={'Profile'}
-                            avatarIcon={"AvatarUserProfile"}
+                            avatarIcon={'AvatarUserProfile'}
                         />
-                 
-                        
-                        {
-                            [1,2,3,4,5,6,7].map(ele=>(
-                        <Avatar
-                            avatarSrc={
-                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
-                            }
-                            styleName={'AvatarUsers'}
-                            name={'ele'}
-                            key={ele}
-                        />
-))}
+
+                        {[1, 2, 3, 4, 5, 6, 7].map((ele) => (
+                            <Avatar
+                                avatarSrc={
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
+                                }
+                                styleName={'AvatarUsers'}
+                                name={'ele'}
+                                key={ele}
+                            />
+                        ))}
                     </div>
+
+                    <br />
+
                     <div className='homePagePostArea'>
-                        <Post/>
-                        </div>
+                        <Post />
+                    </div>
                 </div>
+
                 <div className='homePageProfile'>homePageProfile</div>
             </div>
         </div>

0c3d5cd30238a04564b0b3ee8faac4ef4d6d2664
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-08 09:58:16 +0100

    fix home page allignment

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,45 +1,42 @@
-
 import './Home.css';
 import Avatar from '../../components/Avatar';
+import Body from '../../components/Body/Body';
+import Card from '../../components/card/Card';
 
-
-function Home() {
-
-
+function Home({ bodyStyleName, cardStyleName }) {
     return (
-        <div className='homePage'>
-       
-            <div className='homePageContainer'>
-                <div>
-                    <div className='homePageStory'>
-                       
-                    <Avatar
-                            avatarSrc={
-                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
-                            }
-                            styleName={'AvatarUserProfile'}
-                            name={'Profile'}
-                            avatarIcon={"AvatarUserProfile"}
-                        />
-                 
-                        
-                        {
-                            [1,2,3,4,5,6,7].map(ele=>(
-                        <Avatar
-                            avatarSrc={
-                                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
-                            }
-                            styleName={'AvatarUsers'}
-                            name={'ele'}
-                            key={ele}
-                        />
-))}
+        <Body styleName={bodyStyleName}>
+            <Card styleName={cardStyleName}>
+                <div className='homePage'>
+                    <div className='homePageContainer'>
+                        <div>
+                            <div className='homePageStory'>
+                                <Avatar
+                                    avatarSrc={
+                                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
+                                    }
+                                    styleName={'AvatarUserProfile'}
+                                    name={'Profile'}
+                                    avatarIcon={'AvatarUserProfile'}
+                                />
+                                {[1, 2, 3, 4, 5, 6, 7].map((ele) => (
+                                    <Avatar
+                                        avatarSrc={
+                                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaLtb_3tNc2GjjuNWX29vbxcdvMGOyGEIKaQ&usqp=CAU'
+                                        }
+                                        styleName={'AvatarUsers'}
+                                        name={'ele'}
+                                        key={ele}
+                                    />
+                                ))}
+                            </div>
+                            <div className='homePagePostArea'>Post</div>
+                        </div>
+                        <div className='homePageProfile'>homePageProfile</div>
                     </div>
-                    <div className='homePagePostArea'>Post</div>
                 </div>
-                <div className='homePageProfile'>homePageProfile</div>
-            </div>
-        </div>
+            </Card>
+        </Body>
     );
 }
 

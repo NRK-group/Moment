53cf5220991693e373431ab472b7f471c2bf9917
nater68 <n.d.russell@hotmail.com>
2022-10-10 19:07:23 +0100

    New post page complete

--- frontend/src/features/newpost/NewPost.jsx
+++ frontend/src/features/newpost/NewPost.jsx
@@ -1,4 +1,5 @@
 import Card from "../../components/card/Card";
+import { MessagesIcon } from "../../components/Icons/Icons";
 import PrivacySelector from "../profile/PrivacySelector";
 import './NewPost.css'
 
@@ -10,21 +11,21 @@ export default function NewPost() {
           <span className="newPostTitle">Create a post</span>
           <span className='newPostHeaderCross' >&#10005;</span>
         </Card>
+        
+
         <Card styleName='NewPostContent'>
           <Card styleName='newPostPhotoSection'>
             <Card styleName='newPostImgHolder'>
-
             </Card>
-            <span>
-            <p>Share an image or video ! </p>
-            <button className="newPostImgBtn">+</button>
-            </span>
+            <button className="newPostImgBtn">Select a photo</button>
           </Card>
 
+
           <Card styleName='NewPostContentInput'>
+        <PrivacySelector styleName='newPostPrivacySelector' closeFriends={true} />
+
             <textarea cols='100' rows='7' wrap='hard' className='newPostTextContent' maxlength='280' placeholder="What happened today ?" />
-            <PrivacySelector styleName='newPostPrivacySelector' closeFriends={true} />
-            <button styleName=''>Post</button>
+            <button className='NewPostSendBtn'><span className="shareText">Share</span><MessagesIcon /></button>
           </Card>
 
         </Card>

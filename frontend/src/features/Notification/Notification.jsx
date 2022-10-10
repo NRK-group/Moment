5b33922897fe409b85f71dbd5521145362639054
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-10 07:47:08 +0100

    add media query

--- frontend/src/features/Notification/Notification.jsx
+++ frontend/src/features/Notification/Notification.jsx
@@ -2,8 +2,29 @@ import './Notification.css';
 import { NavLink } from 'react-router-dom';
 import Body from '../../components/Body/Body';
 import Card from '../../components/card/Card';
+import MiniUserCard from '../../components/MiniUserCard/MiniUserCard';
 
 export const Notification = () => {
+    let users = [
+        {
+            id: 1,
+            img: './logo.svg',
+            name: 'Moment',
+            content: 'Liked your post • 1w ago',
+        },
+        {
+            id: 2,
+            img: './logo.svg',
+            name: 'Moment',
+            content: 'Commented on your post • 1w ago',
+        },
+        {
+            id: 3,
+            img: './logo.svg',
+            name: 'Moment',
+            content: 'React on you post • 1w ago',
+        },
+    ];
     return (
         <Body styleName='mobile'>
             <Card styleName='mobileCard'>
@@ -22,7 +43,17 @@ export const Notification = () => {
                             <div className='notificationHeader'>Group</div>
                         </NavLink>
                     </div>
-                    <div className='notificationContentContainer'></div>
+                    <div className='notificationContentContainer'>
+                        {users.map(({ img, id, name, content }) => (
+                            <MiniUserCard
+                                key={id}
+                                img={img}
+                                propsId={`chat` + id}
+                                name={name}
+                                content={content}
+                            />
+                        ))}
+                    </div>
                 </div>
             </Card>
         </Body>

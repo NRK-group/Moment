cc87955d0eca910f10ce7372e5d0a7131543259c
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-12 20:18:23 +0100

    final design

--- frontend/src/features/Chat/Chat.jsx
+++ frontend/src/features/Chat/Chat.jsx
@@ -6,76 +6,57 @@ import { ChatUsersContainer } from './components/chatUsersContainer';
 import { Messages } from '../Messages/Messages';
 const Chat = ({ bodyStyleName, cardStyleName }) => {
     let users = [
-        {
-            name: 'John',
-            img: 'https://picsum.photos/200',
-            id: 1,
-            content: 'Hi',
-            Messages: [{ name: 'John', content: 'Hi' }],
-        },
-        {
-            name: 'John long name long name aojshdjhasldhalskdhklashdlkas',
-            img: 'https://picsum.photos/200',
-            id: 2,
-            content: 'Hi',
-        },
-        {
-            name: 'John',
-            img: 'https://picsum.photos/200',
-            id: 3,
-            content: 'Hi',
-        },
-        {
-            name: 'John',
-            img: 'https://picsum.photos/200',
-            id: 4,
-            content: 'Hi',
-        },
-        {
-            name: 'John',
-            img: 'https://picsum.photos/200',
-            id: 5,
-            content: 'Hi',
-        },
+        // {
+        //     name: 'John',
+        //     img: 'https://picsum.photos/200',
+        //     id: 1,
+        //     content: 'Hi',
+        //     Messages: [{ name: 'John', content: 'Hi' }],
+        // },
+        // {
+        //     name: 'John long name long name aojshdjhasldhalskdhklashdlkas',
+        //     img: 'https://picsum.photos/200',
+        //     id: 2,
+        //     content: 'Hi',
+        // },
+        // {
+        //     name: 'John',
+        //     img: 'https://picsum.photos/200',
+        //     id: 3,
+        //     content: 'Hi',
+        // },
+        // {
+        //     name: 'John',
+        //     img: 'https://picsum.photos/200',
+        //     id: 4,
+        //     content: 'Hi',
+        // },
+        // {
+        //     name: 'John',
+        //     img: 'https://picsum.photos/200',
+        //     id: 5,
+        //     content: 'Hi',
+        // },
     ];
     let currentUserName = 'Moment';
     return (
         <Body styleName={bodyStyleName}>
             <Card styleName={cardStyleName}>
-                {bodyStyleName === 'mobile' ? (
-                    users.length !== 0 ? (
-                        <div className='chatContainer'>
-                            <div className='chatBox'>
-                                <ChatUsersContainer
-                                    styleName='chatUsersContainerMobile'
-                                    users={users}
-                                    currentUserName={currentUserName}
-                                />
-                            </div>
-                        </div>
-                    ) : (
-                        // for mobile view without any previews messages
-                        <div className='chatContainer'>
-                            <div className='chatBox'>
-                                <div className='sendMessageContainer'>
-                                    <Messages />
-                                </div>
-                            </div>
-                        </div>
-                    )
-                ) : (
-                    // for desktop view
-                    <div className='chatContainer'>
-                        <div className='chatBox'>
-                            <ChatUsersContainer
-                                styleName='chatUsersContainerDesktop'
-                                users={users}
-                                currentUserName={currentUserName}
-                            />
-                            <Messages msg={['hello']} name={users[1].name} />
+                <div className='chatContainer'>
+                    <div className='chatBox'>
+                        <ChatUsersContainer
+                            styleName='chatUsersContainerDesktop'
+                            users={users}
+                            currentUserName={currentUserName}
+                        />
+                        <div className='messagesContainer'>
+                            {/* <div className='sendMessageContainer'>
+                                <SendMessageBox />
+                            </div> */}
+                            <Messages msg={[]} name={users} />
                         </div>
                     </div>
-                )}
+                </div>
             </Card>
         </Body>
     );
--- frontend/src/features/Messages/Messages.jsx
+++ frontend/src/features/Messages/Messages.jsx
@@ -5,72 +5,63 @@ import { MessagesIcon, UserIcon } from '../../components/Icons/Icons';
 import { MessageContainer } from './components/messageContainer';
 import { MessageContent } from './components/MessageContent';
 import { ProfileIcon } from '../../components/Icons/Icons';
-import MiniUserCard from '../../components/MiniUserCard/MiniUserCard';
 export const Messages = ({ name, img, msg }) => {
     return (
-        <div className='messagesContainer'>
-            {msg.length === 0 ? (
-                <div className='sendMessageContainer'>
-                    <SendMessageBox />
+        <>
+            <div className='chatMessageContainerHeader'>
+                <div className='messageContainerHeaderContent'>
+                    <div className='messageHeaderInfo'>
+                        <span className='chat'>
+                            {
+                                <ProfileIcon
+                                    iconStyleName='imgIcon'
+                                    imgStyleName='imgIcon'
+                                    img={img}
+                                />
+                            }
+                        </span>
+                        <span className='messageHeaderName longTextElipsis'>
+                            {name}
+                        </span>
+                    </div>
+                    {/* this will be replace by the elipsis btn */}
+                    <MessagesIcon />
                 </div>
-            ) : (
-                <>
-                    <div className='chatMessageContainerHeader'>
-                        <div className='messageContainerHeaderContent'>
-                            <div className='messageHeaderInfo'>
-                                <span className='chat'>
-                                    {
-                                        <ProfileIcon
-                                            iconStyleName='imgIcon'
-                                            imgStyleName='imgIcon'
-                                            img={img}
-                                        />
-                                    }
-                                </span>
-                                <span className='messageHeaderName longTextElipsis'>
-                                    {name}
-                                </span>
-                            </div>
-                            {/* this will be replace by the elipsis btn */}
-                            <MessagesIcon />
-                        </div>
+            </div>
+            <div className='chatMessageContainer scrollbar-hidden'>
+                <MessageContainer name='Firstname' img='./logo.svg'>
+                    <MessageContent
+                        date={'11 October 2022 •17:46'}
+                        content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet aliquam aliquam, nisl nisl aliquam nisl, sit amet aliquam nisl nisl sit amet.'
+                    />
+                </MessageContainer>
+                <MessageContent
+                    containerStyle='senderContainer'
+                    type='sender'
+                    date={'11 October 2022 •17:46'}
+                    content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet aliquam aliquam, nisl nisl aliquam nisl, sit amet aliquam nisl nisl sit amet.'
+                />
+            </div>
+            <div className='messageInputContainer'>
+                {/* this will be replace by the emoji btn */}
+                <div className='inputContainer'>
+                    <div>
+                        <MessagesIcon />
                     </div>
-                    <div className='chatMessageContainer scrollbar-hidden'>
-                        <MessageContainer name='Firstname' img='./logo.svg'>
-                            <MessageContent
-                                date={'11 October 2022 •17:46'}
-                                content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet aliquam aliquam, nisl nisl aliquam nisl, sit amet aliquam nisl nisl sit amet.'
-                            />
-                        </MessageContainer>
-                        <MessageContent
-                            containerStyle='senderContainer'
-                            type='sender'
-                            date={'11 October 2022 •17:46'}
-                            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet aliquam aliquam, nisl nisl aliquam nisl, sit amet aliquam nisl nisl sit amet.'
-                        />
-                    </div>
-                    <div className='messageInputContainer'>
-                        {/* this will be replace by the emoji btn */}
-                        <div className='inputContainer'>
-                            <div>
-                                <MessagesIcon />
-                            </div>
-                            {/* <
+                    {/* <
                         placeholder='message'
                         styleName='messageInput'></Input> */}
-                            <div className='messageInput'>
-                                <textarea
-                                    id=''
-                                    rows='2'
-                                    placeholder='message'></textarea>
-                            </div>
-                            <div>
-                                <MessagesIcon />
-                            </div>
-                        </div>
+                    <div className='messageInput'>
+                        <textarea
+                            id=''
+                            rows='2'
+                            placeholder='message'></textarea>
                     </div>
-                </>
-            )}
-        </div>
+                    <div>
+                        <MessagesIcon />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };

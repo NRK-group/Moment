6c02b63519dfc96d6e5af43f7eb14de3eb73a990
nater68 <n.d.russell@hotmail.com>
2022-10-17 18:33:24 +0100

    Registration URL tests

---
+++ backend/Test/authentication_test.go
@@ -0,0 +1,34 @@
+package Test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"backend/pkg/db/sqlite"
+	"backend/pkg/handler"
+)
+
+func TestRegistration(t *testing.T) {
+	t.Run("Request with valid URL", func(t *testing.T) {
+		// Create the database that will be used for testing
+		database := sqlite.CreateDatabase("./social_network_test.db")
+
+		// migrate the database
+		sqlite.MigrateDatabase("file://../pkg/db/migrations/sqlite", "sqlite3://./social_network_test.db")
+
+		// Create the database struct
+		DB := &handler.DB{DB: database}
+
+		req := httptest.NewRequest(http.MethodPost, "/registration", nil)
+		w := httptest.NewRecorder()
+
+		DB.Registration(w, req)
+		want := 200
+		got := w.Code
+
+		if got != want {
+			t.Errorf("Expected %v got %v", want, got)
+		}
+	})
+}
--- backend/pkg/handler/authentication.go
+++ backend/pkg/handler/authentication.go
@@ -2,6 +2,6 @@ package handler
 
 import "net/http"
 
-func (databse *DB) Registeration(w http.ResponseWriter, r *http.Request) {
+func (DB *DB) Registration(w http.ResponseWriter, r *http.Request) {
 
 }

139f7ca105fc87be14b3dfad055c9dcaa07a7d44
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-17 16:13:51 +0100

    setup the home handler

--- backend/server.go
+++ backend/server.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"backend/pkg/db/sqlite"
+	"backend/pkg/handler"
 )
 
 func main() {
 	db := sqlite.CreateDatabase("./social_network.db")                                         // this open or create the database
 	sqlite.MigrateDatabase("file://pkg/db/migrations/sqlite", "sqlite3://./social_network.db") // migrate the database
-	defer db.Close()                                                                           // close the database
+	database := &handler.DB{DB: db}                                                            // create a database structs
+	http.HandleFunc("/", database.Home)
+	defer db.Close() // close the database
+	fmt.Println("Server is running on port 5070")
+	http.ListenAndServe(":5070", nil)
 	fmt.Println("setup")
 }

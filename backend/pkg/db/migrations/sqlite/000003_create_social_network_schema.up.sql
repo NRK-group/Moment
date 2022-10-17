9f2d1c816dc64bb02b069f6a0e9176598fd73545
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-17 13:06:09 +0100

    add migration version 3

---
+++ backend/pkg/db/migrations/sqlite/000003_create_social_network_schema.up.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS "Event"(
+    "eventId" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "startTime" DATETIME NOT NULL,
+    "endTime" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+        FOREIGN KEY ("userId") 
+            REFERENCES "User" ("userId")
+        FOREIGN KEY ("groupId")
+            REFERENCES "Group" ("groupId")
+);
+CREATE TABLE IF NOT EXISTS "Follower"(
+    "followingId" TEXT NOT NULL,
+    "followerId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    FOREIGN KEY ("followingId") 
+        REFERENCES "User" ("userId"),
+    FOREIGN KEY ("followerId")
+        REFERENCES "User" ("userId")
+);
+CREATE TABLE IF NOT EXISTS "GroupMember"(
+    "groupId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    FOREIGN KEY ("groupId") 
+        REFERENCES "Group" ("groupId"),
+    FOREIGN KEY ("userId")
+        REFERENCES "User" ("userId")
+);
+CREATE TABLE IF NOT EXISTS "EventParticipant"(
+    "eventId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    FOREIGN KEY ("eventId") 
+        REFERENCES "Event" ("eventId"),
+    FOREIGN KEY ("userId")
+        REFERENCES "User" ("userId")
+);
+CREATE TABLE IF NOT EXISTS "Story"(
+    "storyId" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+        FOREIGN KEY ("userId") 
+            REFERENCES "User" ("userId")
+);

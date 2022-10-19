0ae835972914cd83439777fb66aebb6950c91a0c
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-19 12:20:50 +0100

    create migration version 5

--- backend/Test/home_test.go
+++ backend/Test/home_test.go
@@ -1,4 +1,4 @@
-package main
+package Test
 
 import (
 	"net/http"
---
+++ backend/pkg/db/migrations/sqlite/000005_create_social_network_schema.up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS "Message"(
+    messageId TEXT NOT NULL PRIMARY KEY,
+    chatId TEXT,
+    receiverId TEXT,
+    groupId TEXT,
+    senderId TEXT NOT NULL,
+    content TEXT NOT NULL,
+    createdAt DATETIME NOT NULL,
+    FOREIGN KEY ("receiverId")
+        REFERENCES "User"("userId"),
+    FOREIGN KEY ("senderId")
+        REFERENCES "User"("userId")
+);
+CREATE TABLE IF NOT EXISTS "Chat"(
+    chatId TEXT NOT NULL PRIMARY KEY,
+    name TEXT,
+    user1 TEXT NOT NULL,
+    user2 TEXT NOT NULL,
+    FOREIGN KEY ("user1")
+        REFERENCES "User"("userId"),
+    FOREIGN KEY ("user2")
+        REFERENCES "User"("userId")
+);

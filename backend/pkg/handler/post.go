2a4f1d9c5455419f0571ece8e7f423ce02d10a8a
keivon <keivon.991@gmail.com>
2022-10-25 13:29:28 +0100

    add GetBody in post handler

--- backend/pkg/handler/post.go
+++ backend/pkg/handler/post.go
@@ -5,32 +5,28 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"backend/pkg/structs"
+
 	"backend/pkg/post"
+	"backend/pkg/structs"
 )
-
-
 
 func (database *Env) Post(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/post" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
-
 	if r.Method == "GET" {
-		posts, err := post.      AllPost("6t78t8t87", database.Env)
+		posts, err := post.AllPost("6t78t8t87", database.Env)
 		if err != nil {
 			fmt.Print(err)
 			http.Error(w, "500 Internal Server Error.", http.StatusInternalServerError)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-
 		marshallPosts, err := json.Marshal(posts)
 		if err != nil {
 			fmt.Println("Error marshalling the data: ", err)
 		}
-
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(marshallPosts)
@@ -38,14 +34,7 @@ func (database *Env) Post(w http.ResponseWriter, r *http.Request) {
 	}
 	if r.Method == "POST" {
 		var postData structs.Post
-		err := json.NewDecoder(r.Body).Decode(&postData)
-		if err != nil {
-			fmt.Print(err)
-			http.Error(w, "500 Internal Server Error.", http.StatusInternalServerError)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
+		GetBody(&postData, w, r)
 		_, postErr := post.CreatePost(postData.UserID, postData.Content, postData.GroupID, postData.Image, database.Env)
 		fmt.Println("----postData")
 		if postErr != nil {

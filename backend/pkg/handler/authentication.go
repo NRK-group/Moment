ae60c1e316f11c34dfe06d424dcf4bde203dabe6
nater68 <n.d.russell@hotmail.com>
2022-10-25 12:44:32 +0100

    File restructured with struct pkg

--- backend/pkg/handler/authentication.go
+++ backend/pkg/handler/authentication.go
@@ -1,52 +1,15 @@
 package handler
 
 import (
+	"backend/pkg/auth"
+	"backend/pkg/structs"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
 	"net/http"
-	"strings"
-	"time"
-
-	uuid "github.com/satori/go.uuid"
-	"golang.org/x/crypto/bcrypt"
 )
 
-type User struct {
-	UserId       string `json:"UserId"`
-	SessionId    string `json:"SessionId"`
-	FirstName    string `json:"FirstName"`
-	LastName     string `json:"LastName"`
-	NickName     string `json:"NickName"`
-	Email        string `json:"Email"`
-	Password     string `json:"Password"`
-	DateOfBirth  string `json:"DateOfBirth"`
-	AboutMe      string `json:"AboutMe"`
-	Avatar       string `json:"Avatar"`
-	CreatedAt    string `json:"CreatedAt"`
-	IsLoggedIn   int    `json:"IsLoggedIn"`
-	IsPublic     int    `json:"IsPublic"`
-	NumFollowers int    `json:"NumFollowers"`
-	NumFollowing int    `json:"NumFollowing"`
-	NumPosts     int    `json:"NumPosts"`
-}
-
-// Login is a handler which checks if a the login credentials input by a user are valid
-func (DB *DB) Login(w http.ResponseWriter, r *http.Request) {
-	// Confirm the path is correct
-	if r.URL.Path != "/login" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-
-	if r.Method == "POST" {
-		// Check the credentials are correct
-	}
-}
-
 // Registration is a handler where all registration functions are done
-func (DB *DB) Registration(w http.ResponseWriter, r *http.Request) {
+func (DB *Env) Registration(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/registration" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
@@ -54,11 +17,11 @@ func (DB *DB) Registration(w http.ResponseWriter, r *http.Request) {
 
 	// Check if registration is correct
 	if r.Method == "POST" {
-		var newUser User
+		var newUser structs.User
 		// Get the body of the request
 		GetBody(&newUser, w, r)
 		// Insert the new user into the database
-		err := DB.InsertUser(newUser)
+		err := auth.InsertUser(newUser, *DB.Env)
 		if err != nil {
 			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
 			return
@@ -66,47 +29,8 @@ func (DB *DB) Registration(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// InsertUser is a method that inserts a new user into the database
-func (DB DB) InsertUser(newUser User) error {
-	// Create a uuid for the user Id
-	newUser.UserId = uuid.NewV4().String()
-	// Create the sql INSERT statement
-	stmt, err := DB.DB.Prepare(`INSERT INTO User values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
-	if err != nil {
-		fmt.Println("Error preparing inserting user into the db: ", err)
-		return err
-	}
-	// Hash the password and get the current time
-	var hashErr error
-	if !ValidPassword(newUser.Password) {
-		return errors.New("Invalid Password")
-	}
-	newUser.Password, hashErr = HashPassword(newUser.Password)
-	if hashErr != nil {
-		log.Print("Error hashing password", hashErr)
-		return hashErr
-	}
-	newUser.CreatedAt = time.Now().String()
 
-	_, execErr := stmt.Exec(newUser.UserId, "-", newUser.FirstName, newUser.LastName, newUser.NickName, newUser.Email, newUser.DateOfBirth, newUser.Avatar, newUser.AboutMe, newUser.CreatedAt, newUser.IsLoggedIn, newUser.IsPublic, newUser.NumFollowers, newUser.NumFollowing, newUser.NumPosts, newUser.Password)
-	if execErr != nil {
-		fmt.Println("Error executing inserting user into the db: ", execErr)
-		return execErr
-	}
-	return nil
-}
 
-// HashPassword hashes a string so it cannot be read
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
-}
-
-// CheckPasswordHash Checks if a plaintext string and a hashed string are the same
-func CheckPasswordHash(password, hash string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	return err == nil
-}
 
 // GetBody marshalls the body of a request into a struct
 func GetBody(b interface{}, w http.ResponseWriter, r *http.Request) error {
@@ -119,43 +43,5 @@ func GetBody(b interface{}, w http.ResponseWriter, r *http.Request) error {
 	return err
 }
 
-// ValidPassword checks that the password input is valid and passes the requirements
-func ValidPassword(password string) bool {
-	// Check the length of the password is valid
-	if len(password) < 8 || len(password) > 16 {
-		return false
-	}
-	// Check the password contains lower and uppercase values
-	if strings.ToLower(password) == password || strings.ToUpper(password) == password {
-		return false
-	}
-	return true
-}
 
-//
 
-func (DB *DB) GetUser(email, password string) (bool, string) {
-	// Query the db to see if a user exsists with the inpit email
-	rows, err := DB.DB.Query(`SELECT password FROM User WHERE email = ?`, email)
-	if err != nil {
-		fmt.Println("Error querying the db: ", err)
-		return false, "Error querying the db"
-	}
-	counter := 0
-	var pass string
-	for rows.Next() {
-		counter++
-		rows.Scan(&pass)
-	}
-	// If not return false with msg
-	if counter == 0 {
-		return false, "Account not found"
-	}
-	fmt.Println("PASSWORD === ", pass)
-	// Check if the password input is correct
-	if CheckPasswordHash(password, pass) {
-		return true, "Valid Login"
-	}
-	// If not return false with msg
-	return false, "Incorrect Password"
-}

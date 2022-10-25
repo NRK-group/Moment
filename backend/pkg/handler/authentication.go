c8d2a1aec581ae7107e00683d2074165a7aa9b4d
nater68 <n.d.russell@hotmail.com>
2022-10-25 13:31:18 +0100

    Login handler created and errors handled in registration

--- backend/pkg/handler/authentication.go
+++ backend/pkg/handler/authentication.go
@@ -6,18 +6,39 @@ import (
 	"net/http"
 )
 
+//Login is a handler that vlidates the credentials input by a user
+func (DB *Env) Login(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/login" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method == "POST" {
+		var userLogin structs.User
+		err := GetBody(&userLogin, w, r)
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+	}
+}
+
 // Registration is a handler where all registration functions are done
 func (DB *Env) Registration(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/registration" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-
 	// Check if registration is correct
 	if r.Method == "POST" {
 		var newUser structs.User
 		// Get the body of the request
-		GetBody(&newUser, w, r)
+		bodyErr := GetBody(&newUser, w, r)
+		if bodyErr != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 		// Insert the new user into the database
 		err := auth.InsertUser(newUser, *DB.Env)
 		if err != nil {

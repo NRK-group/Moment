29874f210509728160f60ed7fa5113faa5b9bc06
nater68 <n.d.russell@hotmail.com>
2022-10-26 18:32:45 +0100

    Error with cookie funcs

--- backend/Test/logout_test.go
+++ backend/Test/logout_test.go
@@ -1,12 +1,17 @@
 package Test
 
 import (
+	"backend/pkg/auth"
 	"backend/pkg/db/sqlite"
 	"backend/pkg/handler"
 	"backend/pkg/structs"
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestLogout(t *testing.T) {
@@ -53,6 +58,50 @@ func TestLogout(t *testing.T) {
 		if got != want {
 			t.Errorf("Expected %v got %v", want, got)
 		}
+	})
+	t.Run("Remove cookie", func(t *testing.T) {
+		// Create the database that will be used for testing
+		database := sqlite.CreateDatabase("./social_network_test.db")
+
+		// migrate the database
+		sqlite.MigrateDatabase("file://../pkg/db/migrations/sqlite", "sqlite3://./social_network_test.db")
+
+		// Create the database struct
+		DB := &structs.DB{DB: database}
+		Env := handler.Env{Env: DB}
+		randEmail := uuid.NewV4().String()
+		sampleUser := &structs.User{
+			FirstName: "LogoutRemove", LastName: "LogoutRemove", NickName: "LogoutRemove", Email: randEmail, Password: "LogoutRemove1",
+			DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "-", UserId: "-", SessionId: "-",
+			IsLoggedIn: 0, IsPublic: 0, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
+		}
+		err := auth.InsertUser(*sampleUser, *DB)
+		if err != nil {
+			t.Errorf("Error inserting the new user to the db")
+		}
+
+		//Now Log the user in
+		// Create the struct that will be inserted
+		testUser := &structs.User{
+			Email: randEmail, Password: "logoutRemove1",
+		}
+		// Marshal the struct to bytes
+		sampleUserBytes, err := json.Marshal(testUser)
+		if err != nil {
+			t.Errorf("Error marshalling the sampleUser")
+		}
+
+		// Create the bytes into a reader
+		testReq := bytes.NewReader(sampleUserBytes)
+
+		req := httptest.NewRequest(http.MethodPost, "/login", testReq)
+		w := httptest.NewRecorder()
+		Env.Login(w, req)
+
+		//Remove the cookie and check if it has been removed
+		auth.RemoveCookie(w)
+
+		
 	})
 
 }

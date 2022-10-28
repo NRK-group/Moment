35ffd49812d334d9b8bb00a04a4c41f46c8f7f5d
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-10-28 21:28:05 +0100

    automate the creation of the user details on the test

--- backend/Test/follow_test.go
+++ backend/Test/follow_test.go
@@ -1,11 +1,9 @@
 package Test
 
 import (
-	"errors"
 	"testing"
 
 	"backend/pkg/auth"
-	"backend/pkg/db/sqlite"
 	"backend/pkg/follow"
 	"backend/pkg/structs"
 
@@ -15,32 +13,21 @@ import (
 // TestFollow will include all the tests for the follow feature
 func TestFollow(t *testing.T) {
 	// Create the database that will be used for testing
-	database := &structs.DB{DB: sqlite.CreateDatabase("./social_network_test.db")}
-	// migrate the database
-	sqlite.MigrateDatabase("file://../pkg/db/migrations/sqlite", "sqlite3://./social_network_test.db")
+	database := DatabaseSetup()
 	followerId := uuid.NewV4().String()
 	followingId := uuid.NewV4().String()
-	// Create the users that will be used for testing
-	email := logTestEmail
-	inputUser := &structs.User{
-		FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
-		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
-		IsLoggedIn: 0, IsPublic: 1, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
-	}
 	t.Run("Insert follow", func(t *testing.T) {
-		got := follow.InsertFollow(followerId, followingId, database).Error()
-		want := errors.New("no error").Error()
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+		got := follow.InsertFollow(followerId, followingId, database)
+		if got != nil {
+			t.Errorf("got %v, want %v", got, nil)
 		}
 	})
 	followerId = "8a5e7163-7cb7-440a-9537-42a9c4371752"
 	followingId = "9df2d6fb-0ecc-43da-a82e-fd4f4fac51f2"
 	t.Run("Delete follow", func(t *testing.T) {
-		got := follow.DeleteFollow(followerId, followingId, database).Error()
-		want := errors.New("no error").Error()
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+		err := follow.DeleteFollow(followerId, followingId, database)
+		if err != nil {
+			t.Errorf("got %v, want %v", err, nil)
 		}
 	})
 	t.Run("Check follow false", func(t *testing.T) {
@@ -58,44 +45,94 @@ func TestFollow(t *testing.T) {
 			t.Errorf("got %t, want %t", got, want)
 		}
 	})
+	// Create the users that will be used for testing
+	currentEmail := "hello" + uuid.NewV4().String() + "@test.com"
+	currentUser := &structs.User{
+		FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: currentEmail, Password: "Password123",
+		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
+		IsLoggedIn: 0, IsPublic: 1, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
+	}
+	auth.InsertUser(*currentUser, *database)
+	var currentResult structs.User
+	auth.GetUser("email", currentEmail, &currentResult, *database)
 	t.Run("Follow user returns follow", func(t *testing.T) {
-		follow.DeleteFollow("8a5e7163-7cb7-440a-9537-42a9c4371752", "9df2d6fb-0ecc-43da-a82e-fd4f4fac51f2", database)
-		status, err := follow.FollowUser("8a5e7163-7cb7-440a-9537-42a9c4371752", "9df2d6fb-0ecc-43da-a82e-fd4f4fac51f2", database)
-		got := err.Error()
-		want := errors.New("no error").Error()
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+		// Create the users that will be used for testing
+		email := "hello" + uuid.NewV4().String() + "@test.com"
+		inputUser := &structs.User{
+			FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+			DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
+			IsLoggedIn: 0, IsPublic: 1, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
 		}
-		got = status
-		want = "follow"
+		auth.InsertUser(*inputUser, *database)
+		var result structs.User
+		auth.GetUser("email", email, &result, *database)
+		status, err := follow.FollowUser(currentResult.UserId, result.UserId, database)
+		if err != nil {
+			t.Errorf("got %v, want %v", err, nil)
+		}
+		got := status
+		want := "follow"
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
 	t.Run("Follow user returns pending", func(t *testing.T) {
-		if !follow.CheckIfFollow("8a5e7163-7cb7-440a-9537-42a9c4371752", "9df2d6fb-0ecc-43da-a82e-fd4f4fac51f2", database) {
-			follow.InsertFollow("8a5e7163-7cb7-440a-9537-42a9c4371752", "9df2d6fb-0ecc-43da-a82e-fd4f4fac51f2", database)
+		// Create the users that will be used for testing
+		email := "hello" + uuid.NewV4().String() + "@test.com"
+		inputUser := &structs.User{
+			FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+			DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
+			IsLoggedIn: 0, IsPublic: 0, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
 		}
-		status, err := follow.FollowUser("8a5e7163-7cb7-440a-9537-42a9c4371752", "9df2d6fb-0ecc-43da-a82e-fd4f4fac51f2", database)
-		got := err.Error()
-		want := errors.New("no error").Error()
+		auth.InsertUser(*inputUser, *database)
+		var result structs.User
+		auth.GetUser("email", email, &result, *database)
+		status, err := follow.FollowUser(currentResult.UserId, result.UserId, database)
+		if err != nil {
+			t.Errorf("got %v, want %v", err, nil)
+		}
+		got := status
+		want := "pending"
 		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+			t.Errorf("got %v, want %v", got, want)
 		}
-		got = status
-		want = "pending"
+	})
+	t.Run("Follow user returns unfollow", func(t *testing.T) {
+		// Create the users that will be used for testing
+		email := "hello" + uuid.NewV4().String() + "@test.com"
+		inputUser := &structs.User{
+			FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+			DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
+			IsLoggedIn: 0, IsPublic: 0, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
+		}
+		auth.InsertUser(*inputUser, *database)
+		var result structs.User
+		auth.GetUser("email", email, &result, *database)
+		follow.InsertFollow(currentResult.UserId, result.UserId, database)
+		status, err := follow.FollowUser(currentResult.UserId, result.UserId, database)
+		if err != nil {
+			t.Errorf("got %v, want %v", err, nil)
+		}
+		got := status
+		want := "unfollow"
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
-	t.Run("Follow user returns follow", func(t *testing.T) {
+	t.Run("Insert follow notif", func(t *testing.T) {
+		// Create the users that will be used for testing
+		email := "hello" + uuid.NewV4().String() + "@test.com"
+		inputUser := &structs.User{
+			FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+			DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
+			IsLoggedIn: 0, IsPublic: 0, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
+		}
 		auth.InsertUser(*inputUser, *database)
 		var result structs.User
 		auth.GetUser("email", email, &result, *database)
-		got := ""
-		want := "follow"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
+		err := follow.InsertFollowNotif(currentResult.UserId, result.UserId, "pending", database)
+		if err != nil {
+			t.Errorf("got %v, want %v", err, nil)
 		}
 	})
 }
--- backend/pkg/follow/queries.go
+++ backend/pkg/follow/queries.go
@@ -1,9 +1,9 @@
 package follow
 
 import (
-	"errors"
 	"time"
 
+	"backend/pkg/helper"
 	l "backend/pkg/log"
 	"backend/pkg/structs"
 )
@@ -27,7 +27,7 @@ func InsertFollow(follwerId, followingId string, database *structs.DB) error {
 		l.LogMessage("follow.go", "InsertFollow", err)
 		return err
 	}
-	return errors.New("no error")
+	return nil
 }
 
 // DeleteFollow will delete a follow from the database
@@ -48,7 +48,17 @@ func DeleteFollow(followerId, followingId string, database *structs.DB) error {
 		l.LogMessage("follow.go", "DeleteFollow", err)
 		return err
 	}
-	return errors.New("no error")
+	stmt, err = database.DB.Prepare("DELETE FROM FollowNotif WHERE userId = ? AND followingId = ?")
+	if err != nil {
+		l.LogMessage("follow.go", "DeleteFollow", err)
+		return err
+	}
+	_, err = stmt.Exec(followerId, followingId)
+	if err != nil {
+		l.LogMessage("follow.go", "DeleteFollow", err)
+		return err
+	}
+	return nil
 }
 
 // CheckIfFollow checks if current user is following the other user
@@ -76,6 +86,29 @@ func CheckIfFollow(followerId, followingId string, database *structs.DB) bool {
 	return follower != structs.Follower{}
 }
 
+// InsertFollowNotif will insert a follow notification into the database
+//
+// Params:
+//
+//	followerId: the id of the user who is following - current user
+//	followingId: the id of the user who is being followed - other user
+//	status: the status of the follow (follow or pending)
+//	database: the database to insert the follow notification into
+func InsertFollowNotif(followerId, followingId, status string, database *structs.DB) error {
+	createdAt := time.Now().String()
+	stmt, err := database.DB.Prepare("INSERT INTO FollowNotif (userId, followingId, status, createdAt, unread) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		l.LogMessage("follow.go", "InsertFollowNotif", err)
+		return err
+	}
+	_, err = stmt.Exec(followerId, followingId, status, createdAt, true)
+	if err != nil {
+		l.LogMessage("follow.go", "InsertFollowNotif", err)
+		return err
+	}
+	return nil
+}
+
 // FollowUser will follow a user if the user does not already follow the other user
 // and delete the follow if the user does follow the other user
 //
@@ -83,23 +116,19 @@ func CheckIfFollow(followerId, followingId string, database *structs.DB) bool {
 //
 // Params:
 //
-//	followerId: the id of the user who is following
-//	followingId: the id of the user who is being followed
+//	followerId: the id of the user who is following - current user
+//	followingId: the id of the user who is being followed - other user
 //	database: the database to insert the follow into
 func FollowUser(followerId, followingId string, database *structs.DB) (string, error) {
 	if CheckIfFollow(followerId, followingId, database) {
 		DeleteFollow(followerId, followingId, database)
-		return "unfollow", errors.New("no error")
+		return "unfollow", nil
 	}
-	InsertFollow(followerId, followingId, database)
-	return "follow", errors.New("no error")
-}
-
-
-func GetFollowers() {
-	// get all the followers of a user
-}
-
-func GetFollowing() {
-	// get all the users a user is following
+	if helper.CheckUserIfPublic(followingId, database) {
+		InsertFollow(followerId, followingId, database)
+		InsertFollowNotif(followerId, followingId, "follow", database)
+		return "follow", nil
+	}
+	InsertFollowNotif(followerId, followingId, "pending", database)
+	return "pending", nil
 }

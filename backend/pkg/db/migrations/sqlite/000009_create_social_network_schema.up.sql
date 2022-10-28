585d5c6a01b14962fe8e7a14347c57967613a870
nater68 <n.d.russell@hotmail.com>
2022-10-28 12:36:00 +0100

    Close friends table added

--- backend/pkg/db/migrations/sqlite/000009_create_social_network_schema.up.sql
+++ backend/pkg/db/migrations/sqlite/000009_create_social_network_schema.up.sql
@@ -2,4 +2,8 @@ CREATE TABLE IF NOT EXISTS "CloseFriends" (
     "userId" TEXT NOT NULL,
     "closeFriendId" TEXT NOT NULL,
     "CreatedAt" DATETIME NOT NULL
+    FOREIGN KEY ("userId") 
+            REFERENCES "User" ("userId")
+    FOREIGN KEY ("closeFriendId") 
+            REFERENCES "User" ("userId")
 );

dd192af2abf3ff3f80dbccce22b23231729de4b3
nater68 <n.d.russell@hotmail.com>
2022-10-28 16:56:44 +0100

    Unneeded print statement removed

--- frontend/src/components/Validation/Valid.js
+++ frontend/src/components/Validation/Valid.js
@@ -1,9 +1,8 @@
 export default function CheckCookie() {
-    // //Send a get request to check if the user is valid
-    // let valid
-    // fetch("/validate").then(async resp => {
-    //     valid = resp.json()
-    //     console.log("This user is not logged in")
-    // })
-    return false
+    //Send a get request to check if the user is valid
+    let valid
+    fetch("/validate").then(async resp => {
+        valid = resp.json().valid
+    })
+    return valid
 }

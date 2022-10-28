9253982e463058c212d300db2e1b0bbf27bfc127
nater68 <n.d.russell@hotmail.com>
2022-10-28 16:58:57 +0100

    valid initialised as false

--- frontend/src/components/Validation/Valid.js
+++ frontend/src/components/Validation/Valid.js
@@ -1,6 +1,6 @@
 export default function CheckCookie() {
     //Send a get request to check if the user is valid
-    let valid
+    let valid = false
     fetch("/validate").then(async resp => {
         valid = resp.json().valid
     })

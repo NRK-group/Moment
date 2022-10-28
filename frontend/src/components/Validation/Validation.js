f71452fcf7eb7521299fbfef771892c7a8eab5ea
nater68 <n.d.russell@hotmail.com>
2022-10-28 16:13:44 +0100

    Fetching to the validate endpoint

---
+++ frontend/src/components/Validation/Validation.js
@@ -0,0 +1,9 @@
+function CheckCookie() {
+    //Send a get request to check if the user is valid
+    let valid
+    fetch("/validate").then(async resp => {
+        valid = resp.json()
+        console.log("This user is not logged in")
+    })
+    return valid
+}
--- frontend/src/components/Validation/Validation.jsx
+++ frontend/src/components/Validation/Validation.jsx
@@ -1,16 +1,13 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 export default function Validation(props) {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     //Query the endpoint and check if the cookie present is valid
-    let valid = false
-    console.log("YOUR ARE CALLING VALIDATION")
+    let valid = CheckCookie();
+    console.log('YOUR ARE CALLING VALIDATION');
     //if cookie is valid show children else redirect
     useEffect(() => {
-        if (!valid){
-            navigate("/")
-        }
-    }, [valid])
-    return valid 
-    ?? props.children
+        if (!valid) navigate('/');
+    }, [valid]);
+    return valid ?? props.children;
 }

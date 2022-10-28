93d05777e28a70f9333be050b258519effdb1445
nater68 <n.d.russell@hotmail.com>
2022-10-28 12:41:49 +0100

    comma added to up migration 09

--- backend/pkg/db/migrations/sqlite/000009_create_social_network_schema.up.sql
+++ backend/pkg/db/migrations/sqlite/000009_create_social_network_schema.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "CloseFriends" (
     "userId" TEXT NOT NULL,
     "closeFriendId" TEXT NOT NULL,
-    "CreatedAt" DATETIME NOT NULL
+    "CreatedAt" DATETIME NOT NULL,
     FOREIGN KEY ("userId") 
             REFERENCES "User" ("userId")
     FOREIGN KEY ("closeFriendId") 

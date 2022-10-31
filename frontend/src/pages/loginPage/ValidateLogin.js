b82846005179f07a6376a6aa8f081c8de8688bcb
nater68 <n.d.russell@hotmail.com>
2022-10-31 11:25:53 +0000

    Error message returned updated

--- frontend/src/pages/loginPage/ValidateLogin.js
+++ frontend/src/pages/loginPage/ValidateLogin.js
@@ -1,9 +1,9 @@
 function checkCreds() {
     //Check the values inside the login input fields
     const emailInput = document.querySelector('.loginEmailInput'), passwordInput = document.querySelector('.loginPasswordInput')
-    if (passwordInput.value.length < 8 || passwordInput.value.length > 16 || !mixedCase(passwordInput.value)) return [false, "Password must be between 8-16 characters"]//Check the password isn't too short
+    if (passwordInput.value.length < 8 || passwordInput.value.length > 16 || !mixedCase(passwordInput.value)) return [false, "Incorrect Password"]//Check the password isn't too short
     //Check the email is valid
-    if (!ValidateEmail(emailInput.value)) return [false, "invalid email"]
+    if (!ValidateEmail(emailInput.value)) return [false, "Account not found"]
 
 }  
 function ValidateEmail(email) {

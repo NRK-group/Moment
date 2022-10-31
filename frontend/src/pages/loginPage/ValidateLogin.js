ba24fdb8ba31b674a9dffb5f1ec30b939e7f52c8
nater68 <n.d.russell@hotmail.com>
2022-10-31 11:24:40 +0000

    email checked on client side

--- frontend/src/pages/loginPage/ValidateLogin.js
+++ frontend/src/pages/loginPage/ValidateLogin.js
@@ -3,7 +3,14 @@ function checkCreds() {
     const emailInput = document.querySelector('.loginEmailInput'), passwordInput = document.querySelector('.loginPasswordInput')
     if (passwordInput.value.length < 8 || passwordInput.value.length > 16 || !mixedCase(passwordInput.value)) return [false, "Password must be between 8-16 characters"]//Check the password isn't too short
     //Check the email is valid
+    if (!ValidateEmail(emailInput.value)) return [false, "invalid email"]
+
 }  
+function ValidateEmail(email) {
+    var re = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return re.test(email);
+  }
+
 function mixedCase(str) {
     if (str === str.toUpperCase() || str === str.toLowerCase()) return false
     return true

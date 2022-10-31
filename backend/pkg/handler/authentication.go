4ae7e0f86d1d237bf4a13831a3ba2448adb2e69d
nater68 <n.d.russell@hotmail.com>
2022-10-31 18:13:39 +0000

    refs added to reg input fields

--- backend/pkg/handler/authentication.go
+++ backend/pkg/handler/authentication.go
@@ -23,7 +23,7 @@ func (DB *Env) Login(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/login" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
-	} 
+	}
 	SetupCorsResponse(w)
 	if r.Method == "POST" {
 		var userLogin structs.User
--- frontend/src/pages/regPage/Registration.jsx
+++ frontend/src/pages/regPage/Registration.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import Card from '../../components/card/Card'
 import AuthAlternative from '../../features/authentication/AuthAlternative'
 import AuthCard from '../../features/authentication/AuthCard'
@@ -6,23 +7,24 @@ import AuthInput from '../../features/authentication/AuthInput'
 import './Registration.css'
 
 export default function Registration() {
+  let fisrtName = useRef(), lastName = useRef(), nickName = useRef(), aboutMe = useRef(), regEmail = useRef(), regPassword = useRef(), regConfirm = useRef()
   return (
     <AuthCard>
         <Card styleName='authAvatar'>
         <button className='authAvatarBtn'>+</button>
         </Card>
-        <AuthInput type='text' styleName='loginInput' placeholder='First Name' />
-        <AuthInput type='text' styleName='loginInput' placeholder='Last Name' />
-        <AuthInput type='text' styleName='loginInput' placeholder='Nickname' />
+        <AuthInput type='text' styleName='loginInput' placeholder='First Name' refr={fisrtName} />
+        <AuthInput type='text' styleName='loginInput' placeholder='Last Name' refr={lastName}  />
+        <AuthInput type='text' styleName='loginInput' placeholder='Nickname' refr={nickName}  />
         <AuthDateInput styleName='authDate' daySelector='authDay' monthSelector='authMonth' yearSelector='authYear'/>
-        <AuthInput type='textarea' styleName='loginInput' placeholder='About Me' />
-        <AuthInput type='text' styleName='loginInput' placeholder='Email' />
-        <AuthInput type='password' styleName='loginInput' placeholder='Password' />
-        <AuthInput type='password' styleName='loginInput' placeholder='Confirm Password' />
+        <AuthInput type='textarea' styleName='loginInput' placeholder='About Me' refr={aboutMe}  />
+        <AuthInput type='text' styleName='loginInput' placeholder='Email' refr={regEmail}  />
+        <AuthInput type='password' styleName='loginInput' placeholder='Password' refr={regPassword}  />
+        <AuthInput type='password' styleName='loginInput' placeholder='Confirm Password' refr={regConfirm}  />
         <button className="loginInput loginAttemptBtn">Register</button>
 
 
-        <AuthAlternative question='Already have an account?' option='Log in' redirect='/' /> 
+        <AuthAlternative question='Already have an account?' option='Log in' redirect='/'/> 
     </AuthCard>
   )
 }

a6747adcb75e728b40fbbcbfdb7684a7c762326f
nater68 <n.d.russell@hotmail.com>
2022-10-31 15:34:00 +0000

    remove irrelivent import

--- frontend/src/pages/loginPage/Login.jsx
+++ frontend/src/pages/loginPage/Login.jsx
@@ -1,37 +1,59 @@
-import AuthAlternative from "../../features/authentication/AuthAlternative"
-import AuthCard from "../../features/authentication/AuthCard"
-import AuthInput from "../../features/authentication/AuthInput"
-import './Login.css'
-import ValidateLoginAttempt from './ValidateLogin'
-import { useRef } from "react"
-import { useNavigate } from 'react-router-dom';
+import AuthAlternative from '../../features/authentication/AuthAlternative';
+import AuthCard from '../../features/authentication/AuthCard';
+import AuthInput from '../../features/authentication/AuthInput';
+import './Login.css';
+import ValidateLoginAttempt from './ValidateLogin';
+import { useRef } from 'react';
 
-
-import Card from "../../components/card/Card"
+import Card from '../../components/card/Card';
 
 export default function Login() {
-  const navigate = useNavigate();
+    let email = useRef(),
+        password = useRef(),
+        errMsg = useRef();
 
+    return (
+        <AuthCard>
+            <AuthInput
+                type='text'
+                styleName='loginInput loginEmailInput'
+                placeholder='Email'
+                refr={email}
+            />
+            <AuthInput
+                type='password'
+                styleName='loginInput loginPasswordInput'
+                placeholder='Password'
+                refr={password}
+            />
+            <Card styleName='errMsgHolder' refr={errMsg} />
+            <button
+                className='loginInput loginAttemptBtn'
+                onClick={() =>
+                    ValidateLoginAttempt(
+                        email.current.value,
+                        password.current.value,
+                        errMsg.current
+                    )
+                }>
+                Log in
+            </button>
 
+            <p className='externalLogin'>Log in with: </p>
+            <span className='loginIcons'>
+                <button className='externalBtn loginGithub'>
+                    <i className='fa-brands fa-github'></i> Github
+                </button>
+                <button className='externalBtn loginGoogle'>
+                    <i className='fa-brands fa-google'></i> Google
+                </button>
+            </span>
 
-  let email = useRef(), password = useRef(), errMsg = useRef()
-  
-  return (
-
-      <AuthCard >
-          <AuthInput type='text' styleName='loginInput loginEmailInput' placeholder='Email' refr={email} />
-          <AuthInput type='password' styleName='loginInput loginPasswordInput' placeholder='Password' refr={password} />
-          <Card styleName='errMsgHolder' refr= {errMsg}/>
-          <button className="loginInput loginAttemptBtn" onClick={()=>ValidateLoginAttempt(email.current.value, password.current.value, errMsg.current)}>Log in</button>
-
-          <p className="externalLogin">Log in with: </p>
-          <span className="loginIcons">
-          <button className="externalBtn loginGithub"><i className="fa-brands fa-github"></i> Github</button>
-          <button className="externalBtn loginGoogle"><i className="fa-brands fa-google"></i> Google</button>
-          </span>
-
-          <AuthAlternative question={`Don't have an account?`} option='Register' redirect='/register'/>
-      </ AuthCard>
-
-  )
+            <AuthAlternative
+                question={`Don't have an account?`}
+                option='Register'
+                redirect='/register'
+            />
+        </AuthCard>
+    );
 }

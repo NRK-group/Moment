e19bd50b5fd3dff2193b2a07c479fef8b2b0b83b
nater68 <n.d.russell@hotmail.com>
2022-10-31 11:32:14 +0000

    Message to server from login attempt begun

--- frontend/src/pages/loginPage/ValidateLogin.js
+++ frontend/src/pages/loginPage/ValidateLogin.js
@@ -1,10 +1,11 @@
-function checkCreds() {
+const emailInput, passwordInput
+function CheckCreds() {
     //Check the values inside the login input fields
-    const emailInput = document.querySelector('.loginEmailInput'), passwordInput = document.querySelector('.loginPasswordInput')
+    emailInput = document.querySelector('.loginEmailInput'), passwordInput = document.querySelector('.loginPasswordInput')
     if (passwordInput.value.length < 8 || passwordInput.value.length > 16 || !mixedCase(passwordInput.value)) return [false, "Incorrect Password"]//Check the password isn't too short
     //Check the email is valid
     if (!ValidateEmail(emailInput.value)) return [false, "Account not found"]
-
+    return [true, "Valid entry", emailInput, passwordInput]
 }  
 function ValidateEmail(email) {
     var re = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
@@ -16,3 +17,15 @@ function mixedCase(str) {
     return true
 }
 
+function ValidateLoginAttempt() {
+    if (!CheckCreds()[0]) { //Display the error message to client
+        return
+    }
+    
+    const LOGIN_CREDS = {//Make the obj with the login details
+        Email:  emailInput.value,
+        Password: passwordInput.value
+    }
+    //Send the data to the server to be validated by login handler
+    fetch('/login')
+}

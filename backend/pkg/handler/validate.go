560ad21f45ba36e0229d42f2c7b9307cafcc20a7
nater68 <n.d.russell@hotmail.com>
2022-11-01 18:48:50 +0000

    Validate handler created

--- backend/pkg/handler/validate.go
+++ backend/pkg/handler/validate.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"backend/pkg/auth"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -11,7 +13,7 @@ func (database *Env) Validate(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
-
+	fmt.Println("------VALIDATING")
 	if r.Method == "GET" {
 		c, err := r.Cookie("session_token")
 		if err != nil {
@@ -19,6 +21,23 @@ func (database *Env) Validate(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "401 Unauthorized", http.StatusUnauthorized)
 			return
 		}
-		fmt.Println(c)
+		cookie, cErr := auth.SliceCookie(c.Value)
+		if cErr != nil {
+			log.Println("Error slicing the cookie")
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		valid, seshErr := auth.CheckSession(cookie[2], cookie[0], *database.Env)
+		if seshErr != nil {
+			log.Println("Error searching for session")
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		if !valid {
+			auth.RemoveCookie(w)
+			http.Error(w, "401 Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		io.WriteString(w, "Validated")
 	}
 }

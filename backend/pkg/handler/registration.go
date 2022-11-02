aab010b7d0a4192b6b88fc2a69c1166359ca9c80
nater68 <n.d.russell@hotmail.com>
2022-11-02 14:13:29 +0000

    File structure updated for auth handlers

---
+++ backend/pkg/handler/registration.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"net/http"
+
+	"backend/pkg/auth"
+	"backend/pkg/structs"
+)
+
+func SetupCorsResponse(w http.ResponseWriter) {
+	(w).Header().Set("Access-Control-Allow-Origin", "http://localhost:8070")
+	(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+	(w).Header().Set("Access-Control-Allow-Credentials", "true")
+}
+
+
+
+
+// Registration is a handler where all registration functions are done
+func (DB *Env) Registration(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/registration" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	SetupCorsResponse(w)
+	// Check if registration is correct
+	if r.Method == "POST" {
+		var newUser structs.User
+		// Get the body of the request
+		bodyErr := GetBody(&newUser, w, r)
+		if bodyErr != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		// Insert the new user into the database
+		err := auth.InsertUser(newUser, *DB.Env)
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	}
+}

c1301d205b23d7d0a4126f0f60e00e437d488df4
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-03 15:22:37 +0000

    initialize test for general notif

---
+++ backend/Test/generalNotif_test.go
@@ -0,0 +1,13 @@
+package Test
+
+import "testing"
+
+func TestGeneralNotif(t *testing.T) {
+	t.Run("TestGeneralNotif", func(t *testing.T) {
+		got := ""
+		want := ""
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}

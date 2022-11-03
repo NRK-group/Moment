ad1bdd5627996b79a00a1612a8d5a8755db62224
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-03 16:45:02 +0000

    remove the test code

--- frontend/src/features/Chat/Chat.jsx
+++ frontend/src/features/Chat/Chat.jsx
@@ -4,52 +4,16 @@ import Card from '../../components/card/Card';
 import SendMessageBox from '../Messages/components/SendMessageBox';
 import { ChatUsersContainer } from './components/chatUsersContainer';
 import { Messages } from '../Messages/Messages';
-const Chat = ({ bodyStyleName, cardStyleName, socket, user }) => {
-    console.log('chat', socket);
-    let users = [
-        {
-            name: 'John',
-            img: 'https://picsum.photos/200',
-            id: 1,
-            content: 'Hi',
-        },
-        // {
-        //     name: 'John long name long name aojshdjhasldhalskdhklashdlkas',
-        //     img: 'https://picsum.photos/200',
-        //     id: 2,
-        //     content: 'Hi',
-        // },
-        // {
-        //     name: 'John',
-        //     img: 'https://picsum.photos/200',
-        //     id: 3,
-        //     content: 'Hi',
-        // },
-        // {
-        //     name: 'John',
-        //     img: 'https://picsum.photos/200',
-        //     id: 4,
-        //     content: 'Hi',
-        // },
-        // {
-        //     name: 'John',
-        //     img: 'https://picsum.photos/200',
-        //     id: 5,
-        //     content: 'Hi',
-        // },
-    ];
-    let msg = [];
+const Chat = ({ bodyStyleName, cardStyleName, socket }) => {
+    let user = document.cookie.split('=')[1].split('&')[0];
+    let users = [];
+    let messages = [];
     return (
         <Body styleName={bodyStyleName}>
             <Card styleName={cardStyleName}>
                 <div className='chatContainer'>
                     <div className='chatBox'>
                         {
-                            // users.length === 0 && (
-                            //     <div className='sendMessageContainer'>
-                            //         <SendMessageBox />
-                            //     </div>
-                            // )
                             <ChatUsersContainer
                                 styleName='chatUsersContainer'
                                 users={users}
@@ -57,15 +21,18 @@ const Chat = ({ bodyStyleName, cardStyleName, socket, user }) => {
                             />
                         }
                         <div className='messagesContainer'>
-                            {/* <div className='sendMessageContainer'>
-                                <SendMessageBox />
-                            </div> */}
-                            <Messages
-                                currentUserName={user}
-                                msg={msg} // all the messages
-                                name={Math.floor(Math.random() * 5) + 1} //change to current receiver
-                                socket={socket}
-                            />
+                            {messages ? (
+                                <div className='sendMessageContainer'>
+                                    <SendMessageBox />
+                                </div>
+                            ) : (
+                                <Messages
+                                    currentUserName={user}
+                                    msg={[]} // all the messages
+                                    name={Math.floor(Math.random() * 5) + 1} //change to current receiver
+                                    socket={socket}
+                                />
+                            )}
                         </div>
                     </div>
                 </div>

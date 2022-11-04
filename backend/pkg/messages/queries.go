909b6fce6607ea5dee05addd6263e2f93686f281
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-04 13:43:53 +0000

    initialize GetpreviousPrivateChat

---
+++ backend/pkg/messages/queries.go
@@ -0,0 +1,12 @@
+package messages
+
+import "backend/pkg/structs"
+
+// GetPreviousChat returns the previous chat messages
+//
+// Param:
+//
+//	userId: the user id
+//	database: the database
+func GetPreviousPrivateChat(userId string, database *structs.DB) {
+}

e6b62ff4bb0d47185d6bec8e0c8c5db1ac027db8
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-05 19:59:04 +0000

    add modal structure for the new chat modal

--- frontend/src/features/Chat/components/NewChatModal.jsx
+++ frontend/src/features/Chat/components/NewChatModal.jsx
@@ -1,8 +1,36 @@
-export const NewChatModal = () => {
+import Input from '../../../components/Input/Input';
+import { CloseIcon, SearchIcon } from '../../../components/Icons/Icons';
+export const NewChatModal = ({ setIsModalOpen }) => {
     return (
-        <div className='newChatModalContainer'>
-            <div className='newChatModal'>
-                <div>hello</div>
+        <div
+            className='newChatModalContainer'
+            onClick={() => {
+                setIsModalOpen(false);
+            }}>
+            <div
+                className='newChatModal'
+                onClick={(e) => {
+                    e.stopPropagation();
+                }}>
+                <div className='newChatModalHeader'>
+                    <div className='closeIcon'>
+                        <CloseIcon
+                            action={() => {
+                                setIsModalOpen(false);
+                            }}
+                        />
+                    </div>
+                    <div className='newChatModalHeaderSearch'>
+                        <div className='searchContainer'>
+                            <SearchIcon />
+                            <Input
+                                styleName='searchFollowing'
+                                placeholder={'Search'}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div></div>
             </div>
         </div>
     );

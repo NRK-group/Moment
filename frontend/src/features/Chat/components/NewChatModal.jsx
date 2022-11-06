2f4d24b2828bbebe454a0a00818cecadb7ce30f2
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-06 17:33:18 +0000

    change the layout of the new chat modal

--- frontend/src/features/Chat/components/NewChatModal.jsx
+++ frontend/src/features/Chat/components/NewChatModal.jsx
@@ -1,5 +1,6 @@
 import Input from '../../../components/Input/Input';
 import { CloseIcon, SearchIcon } from '../../../components/Icons/Icons';
+import MiniUserCard from '../../../components/MiniUserCard/MiniUserCard';
 export const NewChatModal = ({ setIsModalOpen }) => {
     return (
         <div
@@ -20,17 +21,24 @@ export const NewChatModal = ({ setIsModalOpen }) => {
                             }}
                         />
                     </div>
-                    <div className='newChatModalHeaderSearch'>
-                        <div className='searchContainer'>
-                            <SearchIcon />
-                            <Input
-                                styleName='searchFollowing'
-                                placeholder={'Search'}
-                            />
-                        </div>
-                    </div>
+                    <div className='title'>New Message</div>
+                    <div></div>
                 </div>
-                <div></div>
+                <div className='newChatModalHeaderSearch'>
+                    <div>To:</div>
+                    <Input
+                        styleName='searchFollowing'
+                        placeholder={'Search . . .'}
+                    />
+                </div>
+                <div className='searchResult scrollbar-hidden'>
+                    <MiniUserCard propsId={'1'} name='First'>
+                        <div>online</div>
+                    </MiniUserCard>
+                    <MiniUserCard propsId={'2'} name='Second'>
+                        <div>offline</div>
+                    </MiniUserCard>
+                </div>
             </div>
         </div>
     );

99b9ecd9c687f1fee525f7fdc32615c8c0b956fb
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-08 18:05:47 +0000

    fetch the chat of the user

--- frontend/src/features/Chat/Chat.jsx
+++ frontend/src/features/Chat/Chat.jsx
@@ -5,11 +5,34 @@ import SendMessageBox from '../Messages/components/SendMessageBox';
 import { ChatUsersContainer } from './components/chatUsersContainer';
 import { Messages } from '../Messages/Messages';
 import { NewChatModal } from './components/NewChatModal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 const Chat = ({ bodyStyleName, cardStyleName, socket }) => {
     let [isModalOpen, setIsModalOpen] = useState(false);
     let user = document.cookie.split('=')[1].split('&')[0];
     const [currentReceiver, setcurrentReceiver] = useState('');
+    const [chatList, setChatList] = useState([]);
+    const [receiverinfo, setReceiverInfo] = useState({
+        chatId: '',
+        userId: '',
+        username: '',
+        img: '',
+    });
+    useEffect(() => {
+        getChatList();
+    }, []);
+    const getChatList = () => {
+        fetch('http://localhost:5070/chat', {
+            credentials: 'include',
+        })
+            .then((res) => {
+                console.log(res);
+                return res.json();
+            })
+            .then((data) => {
+                setChatList(data);
+                console.log(data);
+            });
+    };
     return (
         <>
             <>
@@ -27,13 +50,16 @@ const Chat = ({ bodyStyleName, cardStyleName, socket }) => {
                                     currentUserName={user}
                                     setIsModalOpen={setIsModalOpen}
                                     setcurrentReceiver={setcurrentReceiver}
+                                    setReceiverInfo={setReceiverInfo}
+                                    chatList={chatList}
                                 />
                             }
                             <div className='messagesContainer'>
-                                {currentReceiver ? (
+                                {receiverinfo.userId ? (
                                     <Messages
                                         currentUserName={user}
                                         name={currentReceiver}
+                                        receiverinfo={receiverinfo}
                                         socket={socket}
                                     />
                                 ) : (

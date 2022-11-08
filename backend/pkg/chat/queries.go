02fb6fd0528f32633a71f83240993af0985724f1
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-08 18:04:17 +0000

    add getUserInfo query

--- backend/pkg/chat/queries.go
+++ backend/pkg/chat/queries.go
@@ -15,24 +15,36 @@ import (
 //
 //	userId: the user id
 //	database: the database
-func GetPreviousPrivateChat(userId string, database *structs.DB) ([]structs.Chat, error) {
+func GetPreviousPrivateChat(userId string, database *structs.DB) ([]structs.ChatWriter, error) {
 	var prevChat structs.Chat
-	var prevChats []structs.Chat
+	var chatList []structs.ChatWriter
 	row, err := database.DB.Query("SELECT * FROM Chat WHERE user1 = ? or user2 = ?", userId, userId)
 	if err != nil {
 		l.LogMessage("Chat", "GetPreviousPrivateChat - Query", err)
-		return prevChats, err
+		return chatList, err
 	}
 	for row.Next() {
 		err = row.Scan(&prevChat.ChatId, &prevChat.GroupId, &prevChat.User1, &prevChat.User2, &prevChat.UpdatedAt)
 		if err != nil {
 			l.LogMessage("Chat", "GetPreviousPrivateChat - Scan", err)
-			return prevChats, err
+			return chatList, err
 		}
-		prevChats = append([]structs.Chat{prevChat}, prevChats...)
-
+		if prevChat.User1 == userId {
+			userInfo, _ := GetUserInfo(prevChat.User2, database)
+			chatList = append([]structs.ChatWriter{{
+				ChatId: prevChat.ChatId,
+				User:   userInfo,
+			}}, chatList...)
+		} else {
+			userInfo, _ := GetUserInfo(prevChat.User1, database)
+			chatList = append([]structs.ChatWriter{{
+				ChatId: prevChat.ChatId,
+				User:   userInfo,
+			}}, chatList...)
+		}
 	}
-	return prevChats, nil
+	l.LogMessage("Chat", "GetPreviousPrivateChat - ChatList", chatList)
+	return chatList, nil
 }
 
 // InsertNewChat inserts a new chat message
@@ -45,7 +57,7 @@ func GetPreviousPrivateChat(userId string, database *structs.DB) ([]structs.Chat
 func InsertNewChat(user1Id string, user2Id string, database *structs.DB) (string, error) {
 	stmt, err := database.DB.Prepare("INSERT INTO Chat (chatId, user1, user2, groupId, updatedAt) VALUES (?, ?, ?, ?, ?)")
 	chatId := uuid.NewV4().String()
-	updateAt := time.Now().String()
+	updateAt := time.Now()
 	if err != nil {
 		l.LogMessage("Chat", "InsertNewChat - Insert Error", err)
 		return "", err
@@ -78,4 +90,37 @@ func InsertNewGroupChat(groupId string, database *structs.DB) error {
 		return err
 	}
 	return nil
+}
+
+// GetUserInfo returns the user info for the chat writer
+//
+// Param:
+//
+//	userId: the user id
+//	database: the database
+func GetUserInfo(userId string, database *structs.DB) (structs.UserInfo, error) {
+	var userInfo structs.UserInfo
+	var user structs.User
+	stmt, err := database.DB.Query("SELECT userId, firstName, lastName, nickName, avatar FROM User WHERE userId = ?", userId)
+	if err != nil {
+		l.LogMessage("Chat", "GetUserInfo - Query Error", err)
+		return userInfo, err
+	}
+	for stmt.Next() {
+		err = stmt.Scan(&user.UserId, &user.FirstName, &user.LastName, &user.NickName, &user.Avatar)
+		if err != nil {
+			l.LogMessage("Chat", "GetUserInfo - Scan Error", err)
+			return structs.UserInfo{}, err
+		}
+		userInfo = structs.UserInfo{
+			UserId: user.UserId,
+			Img:    user.Avatar,
+		}
+		if user.NickName != "" {
+			userInfo.Username = user.NickName
+		} else {
+			userInfo.Username = user.FirstName + " " + user.LastName
+		}
+	}
+	return userInfo, nil
 }

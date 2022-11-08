5c8207328c048cb224e2e7b927f499fd25f075c2
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-08 18:05:09 +0000

    create chat handler

--- backend/server.go
+++ backend/server.go
@@ -23,22 +23,21 @@ func main() {
 
 	// close the database
 	defer networkDb.Close()
-
+	
 	// initialize the routes
-	http.HandleFunc("/", database.Home) 
-	http.HandleFunc("/post", database.Post) 
-	http.HandleFunc("/group", database.Group) 
-	http.HandleFunc("/event", database.Event) 
-	http.HandleFunc("/login", database.Login) 
-	http.HandleFunc("/logout", database.Logout) 
+	http.HandleFunc("/", database.Home)
+	http.HandleFunc("/post", database.Post)
+	http.HandleFunc("/group", database.Group)
+	http.HandleFunc("/event", database.Event)
+	http.HandleFunc("/login", database.Login)
+	http.HandleFunc("/logout", database.Logout)
 	http.HandleFunc("/registration", database.Registration)
 	http.HandleFunc("/validate", database.Validate)
 	http.HandleFunc("/updateprofileimg", database.UpdateImage)
+	http.HandleFunc("/chat", database.Chat)
 
 	gallery := http.FileServer(http.Dir("./images"))
 	http.Handle("/images/", http.StripPrefix("/images/", gallery)) // handling the CSS
-
-
 
 	// handler for the websocket
 	hub := wSocket.NewHub()

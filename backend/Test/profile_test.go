56bb91dc31e902a7422a97442c19a3641ce49739
nater68 <n.d.russell@hotmail.com>
2022-11-08 15:07:51 +0000

    Profile handler begun

---
+++ backend/Test/profile_test.go
@@ -0,0 +1,43 @@
+package Test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"backend/pkg/handler"
+)
+
+func TestProfile(t *testing.T) {
+	t.Run("Request with valid enpoint", func(t *testing.T) {
+		// Create the database that will be used for testing
+
+		Env := handler.Env{Env: database}
+
+		req := httptest.NewRequest(http.MethodGet, "/login", nil)
+		w := httptest.NewRecorder()
+
+		Env.Profile(w, req)
+		want := 200
+		got := w.Code
+
+		if got != want {
+			t.Errorf("Expected %v got %v", want, got)
+		}
+	})
+	t.Run("Request with invalid enpoint", func(t *testing.T) {
+		// Create the database that will be used for testing
+		Env := handler.Env{Env: database}
+
+		req := httptest.NewRequest(http.MethodGet, "/invalid", nil)
+		w := httptest.NewRecorder()
+
+		Env.Profile(w, req)
+		want := 404
+		got := w.Code
+
+		if got != want {
+			t.Errorf("Expected %v got %v", want, got)
+		}
+	})
+}
---
+++ backend/pkg/handler/profile.go
@@ -0,0 +1,7 @@
+package handler
+
+import "net/http"
+
+func (DB *Env) Profile(w http.ResponseWriter, r *http.Request) {
+	
+}

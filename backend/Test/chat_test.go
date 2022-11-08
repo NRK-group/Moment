c19f365bc7cd3645d4279752a6424fafb8d0023e
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-08 17:48:13 +0000

    create test for the chat handler

--- backend/Test/chat_test.go
+++ backend/Test/chat_test.go
@@ -1,11 +1,17 @@
 package Test
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"backend/pkg/auth"
 	"backend/pkg/chat"
 	"backend/pkg/follow"
+	"backend/pkg/handler"
+	l "backend/pkg/log"
 	"backend/pkg/structs"
 
 	uuid "github.com/satori/go.uuid"
@@ -14,7 +20,7 @@ import (
 func TestChat(t *testing.T) {
 	email := "hello" + uuid.NewV4().String() + "@test.com"
 	user1 := &structs.User{
-		FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+		FirstName: "Adriell", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
 		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
 		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
 	}
@@ -24,7 +30,7 @@ func TestChat(t *testing.T) {
 	auth.GetUser("email", user1.Email, &result1, *database)
 	email = "hello" + uuid.NewV4().String() + "@test.com"
 	user2 := &structs.User{
-		FirstName: "FirstTest", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+		FirstName: "Nate", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
 		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
 		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
 	}
@@ -52,6 +58,64 @@ func TestChat(t *testing.T) {
 		err := chat.InsertNewGroupChat("hello", database)
 		if err != nil {
 			t.Errorf("InsertNewGroupChat() error = %v", err)
+		}
+	})
+	t.Run("Get userinfo for the chat", func(t *testing.T) {
+		userInfo, err := chat.GetUserInfo(result1.UserId, database)
+		l.LogMessage("Test", "userInfo", userInfo)
+		if err != nil {
+			t.Errorf("GetUserInfoForChat() error = %v", err)
+		}
+	})
+}
+
+func TestChatHandler(t *testing.T) {
+	t.Run("Chat handler", func(t *testing.T) {
+		// initialize the database struct with a mock database
+		// database := &structs.DB{DB: sqlite.CreateDatabase("./social_network_test.db")}
+		Env := handler.Env{Env: database}
+		validationValidEmail := "validCreds" + uuid.NewV4().String() + "@test.com"
+		inputUser := &structs.User{
+			FirstName: "Validation", LastName: "Validation", NickName: "TesingChat", Email: validationValidEmail, Password: "Password123",
+			DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "-", SessionId: "-",
+			IsLoggedIn: 0, IsPublic: 0, NumFollowers: 0, NumFollowing: 0, NumPosts: 0,
+		}
+		err := auth.InsertUser(*inputUser, *Env.Env)
+		if err != nil {
+			t.Errorf("Error inserting test struct")
+		}
+		var result structs.User
+		auth.GetUser("email", validationValidEmail, &result, *Env.Env)
+		sampleUser := &structs.User{
+			Email: validationValidEmail, Password: "Password123",
+		}
+		sampleUserBytes, err := json.Marshal(sampleUser)
+		if err != nil {
+			t.Errorf("Error marshalling the sampleUser")
+		}
+		testReq := bytes.NewReader(sampleUserBytes) // Create the bytes into a reader
+		req := httptest.NewRequest(http.MethodPost, "/login", testReq)
+		w := httptest.NewRecorder()
+		Env.Login(w, req)
+		if w.Code != http.StatusOK {
+			t.Errorf("Expected status code %d, got %d", http.StatusOK, w.Code)
+		}
+		nr := httptest.NewRequest(http.MethodGet, "/chat", nil) // create a request
+		nr.Header = http.Header{"Cookie": w.Header()["Set-Cookie"]}
+		nrr := httptest.NewRecorder() // create a response recorder
+		Env.Chat(nrr, nr)             // call the handler
+		l.LogMessage("Test", "UserId", result.UserId)
+		// get cookies
+		cookie, _ := nr.Cookie("session_token")
+		got, _ := auth.SliceCookie(cookie.Value)
+		want := result.UserId
+		l.LogMessage("Test", "got", got)
+		l.LogMessage("Test", "want", want)
+		if got[0] != want {
+			t.Errorf("Expected %s, got %s", want, got)
+		}
+		if nrr.Code != http.StatusOK {
+			t.Errorf("Expected status code %d, got %d", http.StatusOK, nrr.Code)
 		}
 	})
 }

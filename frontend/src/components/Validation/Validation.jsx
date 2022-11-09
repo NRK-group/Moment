32c89506de3bafc735eb7019fa5a0954dc53aaad
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-09 03:07:51 +0000

    convert to hooks

--- frontend/src/components/Validation/Validation.jsx
+++ frontend/src/components/Validation/Validation.jsx
@@ -1,23 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-export default function Validation({auth, children}) {
+export default function Validation(auth) {
+    const [authorised, setAuthorised] = useState(false);
     const navigate = useNavigate();
-    //Query the endpoint and check if the cookie present is valid
-    const [valid, setValid] = useState(false);
-    //if cookie is valid show children else redirect
     useEffect(() => {
-        fetch('http://localhost:5070/validate', { credentials: 'include' })
-            .then((resp) => {
-                return resp.text();
-            })
-            .then((response) => {
-                if (response === 'Validated') {
-                    setValid(true);
-                    auth(true)
-                    return;
-                }
-                navigate('/')
-            });
-    }, []);
-    return valid ? children : null ;
+        fetch('http://localhost:5070/validate', {
+            credentials: 'include',
+        }).then(async (resp) => {
+            const response = await resp.text();
+            if (response !== 'Validated') {
+                setAuthorised(false);
+                navigate('/');
+                return;
+            }
+            setAuthorised(true);
+            return;
+        });
+    }, [auth]);
+    return authorised;
 }

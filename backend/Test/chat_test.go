c159efa3eab4a02852cd3b6183d3beba11df095b
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-13 20:37:50 +0000

    create test for group chats

--- backend/Test/chat_test.go
+++ backend/Test/chat_test.go
@@ -3,6 +3,7 @@ package Test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,13 +11,17 @@ import (
 	"backend/pkg/auth"
 	"backend/pkg/chat"
 	"backend/pkg/follow"
+	"backend/pkg/group"
 	"backend/pkg/handler"
+	l "backend/pkg/log"
+	"backend/pkg/member"
 	"backend/pkg/structs"
 
 	uuid "github.com/satori/go.uuid"
 )
 
 func TestPrivateChat(t *testing.T) {
+
 	email := "hello" + uuid.NewV4().String() + "@test.com"
 	user1 := &structs.User{
 		FirstName: "Adriell", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
@@ -90,6 +95,87 @@ func TestPrivateChat(t *testing.T) {
 	t.Run("Get userinfo for the chat", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("GetUserInfoForChat() error = %v", err)
+		}
+	})
+}
+
+func TestGroupChat(t *testing.T) {
+	email := "hello" + uuid.NewV4().String() + "@test.com"
+	user1 := &structs.User{
+		FirstName: "Adriell", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
+		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
+	}
+
+	auth.InsertUser(*user1, *database)
+	var result1 structs.User
+	err := auth.GetUser("email", user1.Email, &result1, *database)
+	t.Run("Get user", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error getting test struct %v", err)
+		}
+	})
+	email = "hello" + uuid.NewV4().String() + "@test.com"
+	user2 := &structs.User{
+		FirstName: "Nate", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
+		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
+		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
+	}
+	err = auth.InsertUser(*user2, *database)
+	t.Run("Insert user", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error inserting test struct %v", err)
+		}
+	})
+	var result2 structs.User
+	err = auth.GetUser("email", user2.Email, &result2, *database)
+	t.Run("Get user", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error getting test struct %v", err)
+		}
+	})
+	_, err = follow.FollowUser(result1.UserId, result2.UserId, database)
+	t.Run("Follow user", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error following user %v", err)
+		}
+	})
+	_, err = follow.FollowUser(result2.UserId, result1.UserId, database)
+	t.Run("Follow user", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error following user %v", err)
+		}
+	})
+	groupId, err := group.CreateGroup("hello", "testing", result1.UserId, database)
+	fmt.Println(groupId)
+	t.Run("Create group", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error creating group %v", err)
+		}
+	})
+	_, err = member.AddMember(groupId, result2.UserId, database)
+	t.Run("Add member", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error adding member %v", err)
+		}
+	})
+	_, err = chat.GetUserGroups(result1.UserId, database)
+	t.Run("Get user groups", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error getting user groups %v", err)
+		}
+	})
+	_, err = chat.GetAllMembersOfGroup(groupId, database)
+	t.Run("Get all members of group", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error getting all members of group %v", err)
+		}
+	})
+	chatlist, err := chat.GetPreviousGroupChat(result1.UserId, database)
+	l.LogMessage("Test", "GetPreviousGroupChat", chatlist)
+	t.Run("Get previous group chat", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Error getting previous group chat %v", err)
 		}
 	})
 }

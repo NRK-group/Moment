c58f92fdeba6558763163a3b692e2620e46b9feb
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-13 13:32:36 +0000

    modify the struct

--- backend/pkg/structs/messages.go
+++ backend/pkg/structs/messages.go
@@ -22,13 +22,14 @@ type Chat struct {
 }
 
 type ChatWriter struct {
-	ChatId  string              `json:"chatId"`
-	User    map[string]UserInfo `json:"user"`
-	Content Message             `json:"content"`
+	ChatId  string          `json:"chatId"`
+	Details Info            `json:"details"`
+	Member  map[string]Info `json:"member"`
+	Content Message         `json:"content"`
 }
 
-type UserInfo struct {
-	UserId   string `json:"userId"`
-	Username string `json:"username"`
-	Img      string `json:"img"`
+type Info struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Img  string `json:"img"`
 }

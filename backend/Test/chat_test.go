4d255b90997df6df662ca7be7da475762fa83f88
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-14 15:28:18 +0000

    separate the database

--- backend/Test/chat_test.go
+++ backend/Test/chat_test.go
@@ -21,17 +21,18 @@ import (
 )
 
 func TestPrivateChat(t *testing.T) {
-
+	Env := handler.Env{Env: DatabaseSetup()}
+	defer Env.Env.DB.Close()
 	email := "hello" + uuid.NewV4().String() + "@test.com"
 	user1 := &structs.User{
 		FirstName: "Adriell", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
 		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
-		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
+		IsLoggedIn: 0, IsPublic: 1, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
 	}
 
-	auth.InsertUser(*user1, *database)
+	auth.InsertUser(*user1, *Env.Env)
 	var result1 structs.User
-	err := auth.GetUser("email", user1.Email, &result1, *database)
+	err := auth.GetUser("email", user1.Email, &result1, *Env.Env)
 	t.Run("Get user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error getting test struct %v", err)
@@ -41,40 +42,40 @@ func TestPrivateChat(t *testing.T) {
 	user2 := &structs.User{
 		FirstName: "Nate", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
 		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
-		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
+		IsLoggedIn: 0, IsPublic: 1, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
 	}
-	err = auth.InsertUser(*user2, *database)
+	err = auth.InsertUser(*user2, *Env.Env)
 	t.Run("Insert user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error inserting test struct %v", err)
 		}
 	})
 	var result2 structs.User
-	err = auth.GetUser("email", user2.Email, &result2, *database)
+	err = auth.GetUser("email", user2.Email, &result2, *Env.Env)
 	t.Run("Get user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error getting test struct %v", err)
 		}
 	})
-	_, err = follow.FollowUser(result1.UserId, result2.UserId, database)
+	_, err = follow.FollowUser(result1.UserId, result2.UserId, Env.Env)
 	t.Run("Follow user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error following user %v", err)
 		}
 	})
-	_, err = follow.FollowUser(result2.UserId, result1.UserId, database)
+	_, err = follow.FollowUser(result2.UserId, result1.UserId, Env.Env)
 	t.Run("Follow user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error following user %v", err)
 		}
 	})
-	_, err = chat.InsertNewChat(result1.UserId, result2.UserId, database)
+	_, err = chat.InsertNewChat(result1.UserId, result2.UserId, Env.Env)
 	t.Run("Insert new chat", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("InsertNewChat() error = %v", err)
 		}
 	})
-	chats, err := chat.GetPreviousPrivateChat(result1.UserId, database)
+	chats, err := chat.GetPreviousPrivateChat(result1.UserId, Env.Env)
 	t.Run("Get Previous Private chat", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("GetPreviousPrivateChat() error = %v", err)
@@ -85,13 +86,13 @@ func TestPrivateChat(t *testing.T) {
 			t.Errorf("Error expected 1 chat, got %v", len(chats))
 		}
 	})
-	err = chat.InsertNewGroupChat("hello", database)
+	err = chat.InsertNewGroupChat("hello", Env.Env)
 	t.Run("Insert New Group Chat", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("InsertNewGroupChat() error = %v", err)
 		}
 	})
-	_, err = chat.GetUserInfo(result1.UserId, database)
+	_, err = chat.GetUserInfo(result1.UserId, Env.Env)
 	t.Run("Get userinfo for the chat", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("GetUserInfoForChat() error = %v", err)
@@ -100,6 +101,8 @@ func TestPrivateChat(t *testing.T) {
 }
 
 func TestGroupChat(t *testing.T) {
+	Env := handler.Env{Env: DatabaseSetup()}
+	defer Env.Env.DB.Close()
 	email := "hello" + uuid.NewV4().String() + "@test.com"
 	user1 := &structs.User{
 		FirstName: "Adriell", LastName: "LastTest", NickName: "NickTest", Email: email, Password: "Password123",
@@ -107,9 +110,9 @@ func TestGroupChat(t *testing.T) {
 		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
 	}
 
-	auth.InsertUser(*user1, *database)
+	auth.InsertUser(*user1, *Env.Env)
 	var result1 structs.User
-	err := auth.GetUser("email", user1.Email, &result1, *database)
+	err := auth.GetUser("email", user1.Email, &result1, *Env.Env)
 	t.Run("Get user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error getting test struct %v", err)
@@ -121,57 +124,57 @@ func TestGroupChat(t *testing.T) {
 		DateOfBirth: "0001-01-01T00:00:00Z", AboutMe: "Test about me section", Avatar: "testPath", CreatedAt: "", UserId: "", SessionId: "-",
 		IsLoggedIn: 0, IsPublic: 0, NumFollowers: 5, NumFollowing: 5, NumPosts: 0,
 	}
-	err = auth.InsertUser(*user2, *database)
+	err = auth.InsertUser(*user2, *Env.Env)
 	t.Run("Insert user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error inserting test struct %v", err)
 		}
 	})
 	var result2 structs.User
-	err = auth.GetUser("email", user2.Email, &result2, *database)
+	err = auth.GetUser("email", user2.Email, &result2, *Env.Env)
 	t.Run("Get user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error getting test struct %v", err)
 		}
 	})
-	_, err = follow.FollowUser(result1.UserId, result2.UserId, database)
+	_, err = follow.FollowUser(result1.UserId, result2.UserId, Env.Env)
 	t.Run("Follow user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error following user %v", err)
 		}
 	})
-	_, err = follow.FollowUser(result2.UserId, result1.UserId, database)
+	_, err = follow.FollowUser(result2.UserId, result1.UserId, Env.Env)
 	t.Run("Follow user", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error following user %v", err)
 		}
 	})
-	groupId, err := group.CreateGroup("hello", "testing", result1.UserId, database)
+	groupId, err := group.CreateGroup("hello", "testing", result1.UserId, Env.Env)
 	fmt.Println(groupId)
 	t.Run("Create group", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error creating group %v", err)
 		}
 	})
-	_, err = member.AddMember(groupId, result2.UserId, database)
+	_, err = member.AddMember(groupId, result2.UserId, Env.Env)
 	t.Run("Add member", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error adding member %v", err)
 		}
 	})
-	_, err = chat.GetUserGroups(result1.UserId, database)
+	_, err = chat.GetUserGroups(result1.UserId, Env.Env)
 	t.Run("Get user groups", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error getting user groups %v", err)
 		}
 	})
-	_, err = chat.GetAllMembersOfGroup(groupId, database)
+	_, err = chat.GetAllMembersOfGroup(groupId, Env.Env)
 	t.Run("Get all members of group", func(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error getting all members of group %v", err)
 		}
 	})
-	chatlist, err := chat.GetPreviousGroupChat(result1.UserId, database)
+	chatlist, err := chat.GetPreviousGroupChat(result1.UserId, Env.Env)
 	l.LogMessage("Test", "GetPreviousGroupChat", chatlist)
 	t.Run("Get previous group chat", func(t *testing.T) {
 		if err != nil {
@@ -181,7 +184,8 @@ func TestGroupChat(t *testing.T) {
 }
 
 func TestChatHandler(t *testing.T) {
-	Env := handler.Env{Env: database}
+	Env := handler.Env{Env: DatabaseSetup()}
+	defer Env.Env.DB.Close()
 	validationValidEmail := "add" + uuid.NewV4().String() + "@test.com"
 	inputUser := &structs.User{
 		FirstName: "Adriell", LastName: "Validation123", NickName: "", Email: validationValidEmail, Password: "Password123",

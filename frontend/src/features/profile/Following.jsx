dd307e9eacaea089acbec77e07b8c012e346b1e1
nater68 <n.d.russell@hotmail.com>
2022-11-16 22:08:26 +0000

    Comments removed

--- frontend/src/features/profile/Following.jsx
+++ frontend/src/features/profile/Following.jsx
@@ -39,8 +39,6 @@ export default function Following() {
                                         obj.lastName
                                     }
                                     profileId={obj.id}
-                                    // typeVal={'Following'}
-                                    // btnAction={FollowRelationshipUpdate}
                                     crossIcon='none'
                                 />
                             );

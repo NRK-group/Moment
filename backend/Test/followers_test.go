82f394bb5808704959ca6ebfef19239f2ff09e76
nater68 <n.d.russell@hotmail.com>
2022-11-16 11:20:14 +0000

    Test for followers complete

--- backend/Test/followers_test.go
+++ backend/Test/followers_test.go
@@ -1,7 +1,42 @@
 package Test
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"backend/pkg/follow"
+	"backend/pkg/handler"
+	"backend/pkg/structs"
+)
 
 func TestFollowers(t *testing.T) {
-	
+	Env := handler.Env{Env: database}
+
+	userOne := CreateUser(database, t)
+	userTwo := CreateUser(database, t)
+	userThree := CreateUser(database, t)
+	follow.FollowUser(userTwo.UserId, userOne.UserId, database)
+	follow.FollowUser(userThree.UserId, userOne.UserId, database)
+	// Log user one in
+	loginStruct := structs.User{Email: userOne.Email, Password: userOne.Password}
+	logUser, _ := json.Marshal(loginStruct)
+	// Create the bytes into a reader
+	logReq := bytes.NewReader(logUser)
+	req := httptest.NewRequest(http.MethodPost, "/login", logReq)
+	w := httptest.NewRecorder()
+	Env.Login(w, req)
+	followReq := httptest.NewRequest(http.MethodGet, "/followers", nil)
+	followReq.Header = http.Header{"Cookie": w.Header()["Set-Cookie"]}
+	wr := httptest.NewRecorder()
+	Env.Followers(wr, followReq)
+	var got []structs.Info
+	json.Unmarshal(wr.Body.Bytes(), &got)
+	for _, v := range got {
+		if v.Id != userTwo.UserId || v.Id != userThree.UserId {
+			t.Errorf("got %v, want: %v OR %v", v, userThree.UserId, userTwo.UserId)
+		}
+	}
 }

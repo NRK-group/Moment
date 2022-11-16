1d5675dbb5322b2bf2b1416ff5bea2d6a3c14f37
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-16 13:34:41 +0000

    clean

--- backend/pkg/handler/chat.go
+++ backend/pkg/handler/chat.go
@@ -16,17 +16,13 @@ func (database *Env) Chat(w http.ResponseWriter, r *http.Request) {
 	}
 	SetupCorsResponse(w)
 	c, err := r.Cookie("session_token")
-	if err != nil {
+	if err != nil || !auth.ValidateCookie(c, database.Env, w) {
 		log.Println("No cookie found in validate")
 		http.Error(w, "401 Unauthorized", http.StatusUnauthorized)
 		return
 	}
-	if !auth.ValidateCookie(c, database.Env, w) {
-		http.Error(w, "401 Unauthorized", http.StatusUnauthorized)
-		return
-	}
+	cookie, _ := auth.SliceCookie(c.Value)
 	if r.Method == "GET" {
-		cookie, _ := auth.SliceCookie(c.Value)
 		chats, err := chat.GetPreviousPrivateChat(cookie[0], database.Env)
 		if err != nil {
 			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)

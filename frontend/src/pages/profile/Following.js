d2156267da2d1a7f43f82bda4507a319ec198342
nater68 <n.d.russell@hotmail.com>
2022-11-16 22:06:30 +0000

    Followers displayed

--- frontend/src/pages/profile/Following.js
+++ frontend/src/pages/profile/Following.js
@@ -1,5 +1,5 @@
 export default async function GetFollowing() {
-    return await fetch('http://localhost:5070/following', {
+    return await fetch('http://localhost:5070/getfollowing', {
         credentials: 'include',
     }).then(async (response) => {
         return await response.json();

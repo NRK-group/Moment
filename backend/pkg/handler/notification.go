37b07dd97042d5101b55b17112f321a6ba9213c0
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-16 13:35:04 +0000

    handle follow notif type

--- backend/pkg/handler/notification.go
+++ backend/pkg/handler/notification.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"backend/pkg/auth"
+	"backend/pkg/follow"
 	"backend/pkg/response"
+	"backend/pkg/structs"
 )
 
 // Notification is a notifiation handler
@@ -20,17 +22,36 @@ func (DB *Env) Notification(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	SetupCorsResponse(w)
+	c, err := r.Cookie("session_token") // Check if a cookie is present
+	if err != nil || !auth.ValidateCookie(c, DB.Env, w) {
+		response.WriteMessage("No cookie present user unauthorized", "Unauthorised", w)
+		return
+	}
+	cookie, _ := auth.SliceCookie(c.Value)
 	if r.Method == http.MethodGet {
-		c, err := r.Cookie("session_token") // Check if a cookie is present
-		if err != nil || !auth.ValidateCookie(c, DB.Env, w) {
-			response.WriteMessage("No cookie present user unauthorized", "Unauthorised", w)
-			return
-		}
 		notifType := r.URL.Query().Get("notifType")
+		if notifType == "follow" {
+			var notif []structs.FollowerNotif
+			notif, err = follow.GetFollowNotifs(cookie[0], DB.Env)
+			if err != nil {
+				response.WriteMessage("Error getting notifs", "Error", w)
+				return
+			}
+			err = json.NewEncoder(w).Encode(notif)
+			if err != nil {
+				response.WriteMessage("Error encoding notif", "Error", w)
+				return
+			}
+		} else {
+			err = json.NewEncoder(w).Encode(notifType)
+			if err != nil {
+				response.WriteMessage("Error encoding notif", "Error", w)
+				return
+			}
+		}
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		fmt.Print("-------------------", notifType)
-		w.Write([]byte(notifType))
+
 		return
 	}
 	http.Error(w, "Bad request", http.StatusBadRequest)

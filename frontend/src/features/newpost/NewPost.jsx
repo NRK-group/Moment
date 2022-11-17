56226b9cc685345c52288d6e7611fbf5d28acebd
nater68 <n.d.russell@hotmail.com>
2022-11-17 16:50:22 +0000

    File can be uploaded for the new post

--- frontend/src/features/newpost/NewPost.jsx
+++ frontend/src/features/newpost/NewPost.jsx
@@ -1,36 +1,60 @@
-import Card from "../../components/card/Card";
-import { MessagesIcon } from "../../components/Icons/Icons";
-import PrivacySelector from "../profile/PrivacySelector";
-import './NewPost.css'
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Card from '../../components/card/Card';
+import { MessagesIcon } from '../../components/Icons/Icons';
+import PrivacySelector from '../profile/PrivacySelector';
+import './NewPost.css';
 
 export default function NewPost() {
-  return (
-    <Card styleName='popUp'>
-      <Card styleName='newPostBox'>
-        <Card styleName='newPostHeader'>
-          <span className="newPostTitle">Create a post</span>
-          <span className='newPostHeaderCross' >&#10005;</span>
-        </Card>
-        
+    const navigate = useNavigate('');
+    let imgUpload = useRef();
+    return (
+        <Card styleName='popUp'>
+            <Card styleName='newPostBox'>
+                <Card styleName='newPostHeader'>
+                    <span className='newPostTitle'>Create a post</span>
+                    <span
+                        className='newPostHeaderCross'
+                        onClick={() => navigate('/home')}>
+                        &#10005;
+                    </span>
+                </Card>
 
-        <Card styleName='NewPostContent'>
-          <Card styleName='newPostPhotoSection'>
-            <Card styleName='newPostImgHolder'>
+                <Card styleName='NewPostContent'>
+                    <Card styleName='newPostPhotoSection'>
+                        <Card styleName='newPostImgHolder'></Card>
+                        <button className='newPostImgBtn' onClick={() => imgUpload.current.click()}>
+                            Select a photo
+                        </button>
+                        <input
+                            type='file'
+                            className='none'
+                            ref={imgUpload}
+                            onChange={() => {}}
+                        />
+                    </Card>
+
+                    <Card styleName='NewPostContentInput'>
+                        <PrivacySelector
+                            styleName='newPostPrivacySelector'
+                            closeFriends={true}
+                        />
+
+                        <textarea
+                            cols='100'
+                            rows='7'
+                            wrap='hard'
+                            className='newPostTextContent'
+                            maxlength='280'
+                            placeholder='What happened today ?'
+                        />
+                        <button className='NewPostSendBtn'>
+                            <span className='shareText'>Share</span>
+                            <MessagesIcon />
+                        </button>
+                    </Card>
+                </Card>
             </Card>
-            <button className="newPostImgBtn">Select a photo</button>
-          </Card>
-
-
-          <Card styleName='NewPostContentInput'>
-        <PrivacySelector styleName='newPostPrivacySelector' closeFriends={true} />
-
-            <textarea cols='100' rows='7' wrap='hard' className='newPostTextContent' maxlength='280' placeholder="What happened today ?" />
-            <button className='NewPostSendBtn'><span className="shareText">Share</span><MessagesIcon /></button>
-          </Card>
-
         </Card>
-
-      </Card>
-    </Card>
-  )
+    );
 }

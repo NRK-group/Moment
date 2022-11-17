6c89070f3b7051af93e74afc751cd1297dba0356
nater68 <n.d.russell@hotmail.com>
2022-11-17 19:31:00 +0000

    Login test completed

--- backend/pkg/handler/login.go
+++ backend/pkg/handler/login.go
@@ -44,6 +44,7 @@ func (DB *Env) Login(w http.ResponseWriter, r *http.Request) {
 		}
 		var result structs.User
 		auth.GetUser("email", userLogin.Email, &result, *DB.Env)
+		result.Message = validationMsg
 		data, marshErr := json.Marshal(result)
 		if marshErr != nil {
 			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)

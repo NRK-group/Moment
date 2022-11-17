19a1e7ab7ca8c39d6b293b971e82eed5b124fcc3
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-17 23:13:14 +0000

    hide the delay

--- frontend/src/features/Notification/components/NotificationContentContainer.jsx
+++ frontend/src/features/Notification/components/NotificationContentContainer.jsx
@@ -1,9 +1,12 @@
-import { NavLink, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import MiniUserCard from '../../../components/MiniUserCard/MiniUserCard';
 import { NoNotifications } from './NoNotifications';
 import { CalculateTimeDiff } from '../hooks/calculateTimediff';
 import { GetNotif } from '../hooks/getNotif';
-export const NotificationContentContainer = () => {
+import { Button } from '../../../components/Button/Button';
+import { useState } from 'react';
+import { useEffect } from 'react';
+export const NotificationContentContainer = ({ socket }) => {
     const { type } = useParams();
     const message = {
         follow: 'started following you •',
@@ -13,25 +16,90 @@ export const NotificationContentContainer = () => {
         follow: `/profile?id=`,
         group: `/group?id=`,
     };
-    let notif = GetNotif(type);
+    const [notifications, setNotifications] = useState();
+    GetNotif(type, setNotifications);
+    const handleAction = ({ type, receiverId, senderId }) => {
+        if (socket) {
+            if (type === 'acceptFollowRequest') {
+                socket.send(
+                    JSON.stringify({
+                        type: type,
+                        receiverId: receiverId,
+                        senderId: senderId,
+                    })
+                );
+                let newNotif = notifications.map((notif) => {
+                    if (notif.userId.id === receiverId) {
+                        notif.status = 'follow';
+                    }
+                    return notif;
+                });
+                setNotifications(newNotif);
+            }
+            if (type === 'declineFollowRequest') {
+                socket.send(
+                    JSON.stringify({
+                        type: type,
+                        receiverId: receiverId,
+                        senderId: senderId,
+                    })
+                );
+                let newNotif = notifications.filter(
+                    (notif) => notif.userId.id !== receiverId
+                );
+                setNotifications(newNotif);
+            }
+        }
+    };
     return (
         <div className='notificationContentContainer'>
-            {Array.isArray(notif) && notif.length !== 0 ? (
-                notif.map(({ userId, status, createdAt }) => {
-                    return (
-                        <NavLink to={link[type] + userId.id}>
+            {Array.isArray(notifications) && notifications.length !== 0 ? (
+                notifications.map(
+                    ({ userId, followingId, status, createdAt }) => {
+                        return (
                             <MiniUserCard
                                 key={userId.id}
                                 img={userId.Img}
                                 propsId={`notif` + userId.id}
-                                name={userId.name}>
+                                name={userId.name}
+                                button={
+                                    status === 'pending' ? (
+                                        <>
+                                            <Button
+                                                styleName={'acceptBtn'}
+                                                content={'accept'}
+                                                action={() => {
+                                                    return handleAction({
+                                                        type: 'acceptFollowRequest',
+                                                        receiverId: userId.id,
+                                                        senderId:
+                                                            followingId.id,
+                                                    });
+                                                }}
+                                            />
+                                            <Button
+                                                styleName={'declineBtn'}
+                                                content={'decline'}
+                                                action={() => {
+                                                    handleAction({
+                                                        type: 'declineFollowRequest',
+                                                        receiverId: userId.id,
+                                                        senderId:
+                                                            followingId.id,
+                                                    });
+                                                }}
+                                            />
+                                        </>
+                                    ) : null
+                                }
+                                link={link[type] + userId.id}>
                                 {message[status] +
                                     ' ' +
                                     CalculateTimeDiff(createdAt)}
                             </MiniUserCard>
-                        </NavLink>
-                    );
-                })
+                        );
+                    }
+                )
             ) : (
                 <NoNotifications />
             )}

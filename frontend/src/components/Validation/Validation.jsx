8c76c3f3219bc5153b5e56a4d9a60a70135f3051
nater68 <n.d.russell@hotmail.com>
2022-11-18 19:34:01 +0000

    import removed

--- frontend/src/components/Validation/Validation.jsx
+++ frontend/src/components/Validation/Validation.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 export default function Validation(auth) {
-    // const [authorised, setAuthorised] = useState(false);
     const navigate = useNavigate();
     useEffect(() => {
         fetch('http://localhost:5070/validate', {
--- frontend/src/layouts/Menu/LogoutComp.jsx
+++
@@ -1,8 +0,0 @@
-import { useNavigate } from 'react-router-dom';
-import Logout from './Logout';
-export default function LogoutComp({ auth }) {
-    const navigate = useNavigate();
-
-    Logout(navigate, auth);
-    return <div>You are being Logged out</div>;
-}

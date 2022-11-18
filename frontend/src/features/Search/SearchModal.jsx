23b772a50617e90cf4d6623ebc1e4819a52ca37b
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-18 19:28:45 +0000

    separate hooks

--- frontend/src/features/Search/SearchModal.jsx
+++ frontend/src/features/Search/SearchModal.jsx
@@ -2,21 +2,11 @@ import { useEffect, useState } from 'react';
 import { SearchResultsContainer } from './components/SearchResultsContainer';
 import './Search.css';
 export const SearchModal = ({ query }) => {
-    const [searchResult, setSearchResult] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:5070/search', { credentials: 'include' }).then(
-            async (resp) => {
-                const data = await resp.json();
-                setSearchResult(data);
-            }
-        );
-    }, []);
     return (
         <div className='searchModalContainer'>
             <div className='searchModal'>
                 <div className='searchContainer'>
                     <SearchResultsContainer
-                        searchResult={searchResult}
                         query={query}
                     />
                 </div>

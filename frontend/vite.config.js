1f9e3a82477158fe8007d1645f2d0e8bfc1436cf
nater68 <n.d.russell@hotmail.com>
2022-11-19 13:10:20 +0000

    docker builds with makefile cmds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
         port: 8070,
     },
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        },
+        chunkSizeWarningLimit: 600,
+    },
 });

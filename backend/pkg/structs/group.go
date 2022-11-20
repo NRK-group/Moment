4a033172ad52d8df1fca213e7e4df17dce9fe80a
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-20 23:02:02 +0000

    add struct for group notif

--- backend/pkg/structs/group.go
+++ backend/pkg/structs/group.go
@@ -1,5 +1,7 @@
 package structs
 
+import "time"
+
 type Group struct {
 	CreatedAt   string   `json:"CreatedAt"`
 	Name        string   `json:"Name"`
@@ -8,4 +10,22 @@ type Group struct {
 	Description string   `json:"Description"`
 	Admin       string   `json:"Admin"`
 	Members     []Member `json:"Members"`
+}
+type GroupNotif struct {
+	GroupID    string    `json:"groupId"`
+	UserId     string    `json:"userId"`
+	ReceiverId string    `json:"receiverId"`
+	CreatedAt  time.Time `json:"createdAt"`
+	Type       string    `json:"type"`
+	Status     string    `json:"status"`
+	Read       int       `json:"read"`
+}
+type GroupNotifWriter struct {
+	GroupId    Info      `json:"groupId"`
+	UserId     Info      `json:"userId"`
+	ReceiverId Info      `json:"receiverId"`
+	CreatedAt  time.Time `json:"createdAt"`
+	NotifType  string    `json:"type"`
+	Status     string    `json:"status"`
+	Read       int       `json:"read"`
 }

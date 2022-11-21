5ac1918c0ebd96f4ac0e310e7eeb611bb67a3c22
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-21 01:41:16 +0000

    handle the buttons on the front end

--- frontend/src/features/Notification/components/NotificationContentContainer.jsx
+++ frontend/src/features/Notification/components/NotificationContentContainer.jsx
@@ -1,7 +1,5 @@
 import { useParams } from 'react-router-dom';
 import { NoNotifications } from './NoNotifications';
-import { GetNotif } from '../hooks/getNotif';
-import { useState } from 'react';
 import { FollowNotif } from './followNotif';
 import { GroupNotif } from './GroupNotif';
 export const NotificationContentContainer = ({ socket }) => {
@@ -10,7 +8,7 @@ export const NotificationContentContainer = ({ socket }) => {
         <div className='notificationContentContainer scrollbar-hidden'>
             <>
                 {type === 'follow' ? <FollowNotif socket={socket} /> : null}
-                {type === 'group' ? <GroupNotif /> : null}
+                {type === 'group' ? <GroupNotif socket={socket} /> : null}
                 {type === 'general' ? <NoNotifications /> : null}
             </>
         </div>

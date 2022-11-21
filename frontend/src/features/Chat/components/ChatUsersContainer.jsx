ae1a4e5c3fb679d40eadcdf52829b9648dffddbb
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-11-21 16:08:39 +0000

    handle the deletion of the notif

--- frontend/src/features/Chat/components/ChatUsersContainer.jsx
+++ frontend/src/features/Chat/components/ChatUsersContainer.jsx
@@ -1,17 +1,40 @@
 import ChatContainerHeader from './ChatContainerHeader';
 import MiniUserCard from '../../../components/MiniUserCard/MiniUserCard';
 import { NavLink, Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 export const ChatUsersContainer = ({
     currentUserInfo,
     styleName,
     chatList,
+    socket,
 }) => {
+    const [newMessage, setNewMessage] = useState([]);
+    useEffect(() => {
+        setNewMessage(chatList);
+    }, [chatList]);
+    const handleOpenChat = (chatId) => {
+        socket.send(
+            JSON.stringify({
+                type: 'deleteNotif',
+                chatId: chatId,
+                receiverId: currentUserInfo,
+            })
+        );
+        setNewMessage(
+            newMessage.map((msg) => {
+                if (msg.chatId === chatId) {
+                    msg.notif = 0;
+                }
+                return msg;
+            })
+        );
+    };
     return (
         <div className={`chatUsersContainer ${styleName}`}>
             <ChatContainerHeader userName={currentUserInfo} />
             <div className='chatUsers scrollbar-hidden'>
-                {Array.isArray(chatList)
-                    ? chatList.map(
+                {Array.isArray(newMessage)
+                    ? newMessage.map(
                           ({
                               type,
                               chatId,
@@ -21,30 +44,35 @@ export const ChatUsersContainer = ({
                               notif,
                           }) => {
                               return (
-                                  <NavLink
-                                      key={details.id}
-                                      to={`/messages/${chatId}`}
-                                      state={{
-                                          type: type,
-                                          details: details,
-                                          user: member,
+                                  <div
+                                      onClick={() => {
+                                          handleOpenChat(chatId);
                                       }}>
-                                      <MiniUserCard
-                                          img={details.img}
-                                          propsId={`chat` + details.id}
-                                          name={details.name}
-                                          button={
-                                              <>
-                                                  {notif && notif != 0 ? (
-                                                      <div className='messageNotifIndicator'>
-                                                          {notif}
-                                                      </div>
-                                                  ) : null}
-                                              </>
-                                          }>
-                                          {content.content}
-                                      </MiniUserCard>
-                                  </NavLink>
+                                      <NavLink
+                                          key={details.id}
+                                          to={`/messages/${chatId}`}
+                                          state={{
+                                              type: type,
+                                              details: details,
+                                              user: member,
+                                          }}>
+                                          <MiniUserCard
+                                              img={details.img}
+                                              propsId={`chat` + details.id}
+                                              name={details.name}
+                                              button={
+                                                  <>
+                                                      {notif && notif != 0 ? (
+                                                          <div className='messageNotifIndicator'>
+                                                              {notif}
+                                                          </div>
+                                                      ) : null}
+                                                  </>
+                                              }>
+                                              {content.content}
+                                          </MiniUserCard>
+                                      </NavLink>
+                                  </div>
                               );
                           }
                       )

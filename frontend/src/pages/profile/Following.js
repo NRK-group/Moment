654bc47cf23dad3a56a0aa5d1c6786a90a6c23a3
Nathaniel (Nate) Russell <n.d.russell@hotmail.com>
2022-11-22 13:45:55 +0000

    Delete frontend/src/pages directory

--- frontend/src/pages/profile/Following.js
+++
@@ -1,7 +0,0 @@
-export default async function GetFollowing() {
-    return await fetch('http://localhost:5070/getfollowing', {
-        credentials: 'include',
-    }).then(async (response) => {
-        return await response.json();
-    });
-}

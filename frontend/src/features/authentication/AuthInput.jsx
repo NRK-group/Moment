1882a39d87e49c1bde759341e74781e895f63f0d
Nathaniel (Nate) Russell <n.d.russell@hotmail.com>
2022-11-22 13:45:04 +0000

    Delete frontend/src/features directory

--- frontend/src/features/authentication/AuthInput.jsx
+++
@@ -1,9 +0,0 @@
-export default function AuthInput(props) {
-  return (
-    <div className="inputBox">
-      <input type={props.type} className={props.styleName} ref={props.refr} value={props.value} required />
-      <span className='inputText' >{props.placeholder}</span>
-    </div>
-
-  )
-}
--- frontend/src/features/profile/UserImg.jsx
+++
@@ -1,9 +0,0 @@
-import Card from '../../Components/Card/Card';
-
-export default function UserImg(props) {
-    return (
-        <Card styleName={props.userImgHolder}>
-            <img className={props.profileImg} src={props.src} />
-        </Card>
-    );
-}

299b2ae7f4ccdc73b4416c677588a65cefb57eae
nater68 <n.d.russell@hotmail.com>
2022-11-25 22:28:26 +0000

    Layout added

---
+++ frontend/src/layouts/Menu/Logout.js
@@ -0,0 +1,16 @@
+export default function Logout(redir, setauth) {
+    fetch('http://localhost:5070/logout', {
+        credentials: 'include',
+    })
+        .then(async (response) => {
+            return await response.text();
+        })
+        .then((resp) => {
+            if (resp === 'Logged Out') {
+                setauth(false)
+                redir('/')
+                return true;
+            }
+            return false;
+        });
+}
